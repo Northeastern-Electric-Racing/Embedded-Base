53a2be72e64aeafaeab830adc79b47e168ae3f4d
dyldonahue <98500199+dyldonahue@users.noreply.github.com>
2023-11-05 19:18:04 -0500

    fixed static array (#31)
    Co-authored-by: Dylan Donahue <dmdonahue99@hotmail.com>

--- general/src/m24c32.c
+++ general/src/m24c32.c
@@ -45,14 +45,15 @@ HAL_StatusTypeDef eeprom_delete(uint16_t mem_address, uint16_t size)
     HAL_StatusTypeDef status;
 
     //Create a list of zeroes that will be written to the EEPROM
-    uint8_t data[size] = {0};
+    uint8_t data[size];
+    memset(data, 0, size);
 
     for(int bytes_deleted = 0; bytes_deleted < size; bytes_deleted += 32)
     {
         uint16_t delete_size = check_size(size - bytes_deleted);
 
         //Write 0 to memory addresses
-        status = HAL_I2C_Mem_Write(i2c_handle, M24C32_I2C_ADDR, mem_addr + bytes_deleted, 2, &data[bytes_deleted], 
+        status = HAL_I2C_Mem_Write(i2c_handle, M24C32_I2C_ADDR, mem_address + bytes_deleted, 2, &data[bytes_deleted], 
                                    delete_size, 1000);
         if (status)
             return status;
