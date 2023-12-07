f7fe56fda4c8a950d5aea5cfad738f6cb45117cf
Dylan Donahue <dmdonahue99@hotmail.com>
2023-12-06 21:04:19 -0500

    can init and send function devved

--- platforms/stm32f405/include/can.h
+++ platforms/stm32f405/include/can.h
@@ -5,20 +5,22 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_can.h"
 typedef struct{
     CAN_HandleTypeDef *hcan;
     uint16_t *id_list;
     uint8_t size;
-    HAL_CAN_RxFifo0MsgPendingCallback RxCallback;
+
+    /* desired behavior varies by app - so implement this at app level */
+    void (*can_callback)(CAN_HandleTypeDef *hcan);
 } can_t;
 
-
-
-
-#include "stm32f4xx_hal.h"
-#include "stm32f4xx_hal_can.h"
-
 void can_init(can_t *can);
+void can_send_msg(can_t *can, uint16_t id, uint8_t *data, uint8_t size);
+
+
+
 
 
 
