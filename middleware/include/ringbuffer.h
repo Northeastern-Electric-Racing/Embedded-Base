8a8b0106214f7a6224449781103eb3ef82bb1888
Dylan Donahue <dmdonahue99@hotmail.com>
2023-12-15 19:48:41 -0500

    ringbuffer + gitignore

---
+++ middleware/include/ringbuffer.h
@@ -0,0 +1,26 @@
+#ifndef RINGBUFFER_H
+#define RINGBUFFER_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#endif // RINGBUFFER_H
+
+#define BUFFER_SIZE 10
+
+typedef struct {
+    uint8_t* buffer[BUFFER_SIZE];
+    size_t front;
+    size_t rear;
+    size_t count;
+} ring_buffer_t;
+
+void initialize_ring_buffer(ring_buffer_t* rb);
+
+int is_buffer_empty(ring_buffer_t* rb);
+
+int is_buffer_full(ring_buffer_t* rb);
+
+int enqueue(ring_buffer_t* rb, uint8_t* data);
+
+uint8_t* dequeue(ring_buffer_t* rb);
---
+++ middleware/src/ringbuffer.c
@@ -0,0 +1,48 @@
+#include "ringbuffer.h"
+
+void initialize_ring_buffer(ring_buffer_t* rb)
+{
+    rb->front = 0;
+    rb->rear = 0;
+    rb->count = 0;
+}
+
+int is_buffer_empty(ring_buffer_t* rb) 
+{
+    return rb->count == 0;
+}
+
+int is_buffer_full(ring_buffer_t* rb) 
+{
+    return rb->count == BUFFER_SIZE;
+}
+
+int enqueue(ring_buffer_t* rb, uint8_t* data) 
+{
+    if (is_buffer_full(rb)) {
+
+        /* Buffer is full, cannot enqueue */
+        return -1;
+    }
+
+    rb->buffer[rb->rear] = data;
+    rb->rear = (rb->rear + 1) % BUFFER_SIZE;
+    rb->count++;
+
+    return 0; // Successful enqueue
+}
+
+uint8_t* dequeue(ring_buffer_t* rb) 
+{
+    if (is_buffer_empty(rb)) {
+
+        /* Buffer is empty, cannot dequeue */
+        return NULL;
+    }
+
+    uint8_t* data = rb->buffer[rb->front];
+    rb->front = (rb->front + 1) % BUFFER_SIZE;
+    rb->count--;
+
+    return data;
+}
