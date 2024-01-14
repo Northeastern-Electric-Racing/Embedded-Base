a740b0ea34cf0e55b11f0786b03e51db88c126c7
Scott A <89099102+Sabramz@users.noreply.github.com>
2024-01-14 18:53:54 -0500

    FDCAN drivers for stm32g4 (#60)
    * fdcan drivers for stm32g4
    
    * RxFifo0Callback fixed
    
    ---------
    
    Co-authored-by: Scott A <89099102+Scotab@users.noreply.github.com>

---
+++ platforms/stm32g431/include/fdcan.h
@@ -0,0 +1,32 @@
+#ifndef CAN_H
+#define CAN_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "stm32g4xx_hal.h"
+#include "stm32g4xx_hal_fdcan.h"
+
+/* function pointer type for the callback */
+typedef void (*can_callback_t)(FDCAN_HandleTypeDef *hcan);
+
+typedef struct{
+	FDCAN_HandleTypeDef *hcan;
+	const uint16_t *id_list;
+	uint8_t id_list_len;
+
+	/* desired behavior varies by app - so implement this at app level */
+	can_callback_t callback;
+} can_t;
+
+typedef struct{
+	uint16_t id;
+	uint8_t data[8];
+	uint8_t len;
+} can_msg_t;
+
+HAL_StatusTypeDef can_init(can_t *can);
+HAL_StatusTypeDef can_send_msg(can_t *can, can_msg_t *msg);
+
+#endif // CAN_H
