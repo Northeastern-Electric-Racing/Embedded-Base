9de853d019405aff90ad08510dad8f960e0be39a
hamzaiqbal69 <iqbal.ha@northeastern.edu>
2024-01-29 23:56:17 -0500

    Made interrupts work

--- platforms/stm32f405/src/can.c
+++ platforms/stm32f405/src/can.c
@@ -4,6 +4,7 @@
 
 /* NOTE: STM32F405 will have MAX of 3 CAN buses */
 #define MAX_CAN_BUS	3
+extern CAN_HandleTypeDef hcan1;
 
 can_t *can_struct_list[MAX_CAN_BUS] = {NULL, NULL, NULL};
 
@@ -38,7 +39,7 @@ static uint8_t add_interface(can_t *interface)
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
 	/* Handle CAN reception event */
-	can_callback_t callback= find_callback(hcan);
+	can_callback_t callback = find_callback(hcan);
 
 	if (callback != NULL)
 	{
@@ -93,12 +94,16 @@ HAL_StatusTypeDef can_init(can_t *can)
 		return err;
 
 	/* set up interrupt & activate CAN */
+	HAL_CAN_IRQHandler(can->hcan);
+	
+	err = HAL_CAN_ActivateNotification(can->hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
+	if (err != HAL_OK)
+		return err;
 	err = HAL_CAN_Start(can->hcan);
 	if (err != HAL_OK)
 		return err;
 
 	/* Override the default callback for CAN_IT_RX_FIFO0_MSG_PENDING */
-	err = HAL_CAN_ActivateNotification(can->hcan, CAN_IT_RX_FIFO0_MSG_PENDING);
 	err = add_interface(can);
 
 	return err;
@@ -122,4 +127,9 @@ HAL_StatusTypeDef can_send_msg(can_t *can, can_msg_t *msg)
 		return HAL_ERROR;
 
 	return HAL_OK;
+}
+
+void CAN1_RX0_IRQHandler(void)
+{
+	HAL_CAN_IRQHandler(&hcan1);
 }
