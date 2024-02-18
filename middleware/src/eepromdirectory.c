5f1fe7f71e632f48c90f5a615ff7c3c80fb374e3
Bharath Varma Mudunuru <bharath@Bharaths-MacBook-Pro.local>
2024-02-18 18:38:11 -0500

    Included function for creating parition

--- middleware/src/eepromdirectory.c
+++ middleware/src/eepromdirectory.c
@@ -1,6 +1,38 @@
 #include "eepromdirectory.h"
 #include "general/include/m24c32.h"
 
+
+/*   user defined data type for taking in
+  1. id of each fault
+  2. size of the fault
+ */
+struct eeprrom_struct
+{
+   char id;
+   int size;
+}
+
+/* taking in the number of user */
+int size_of_array = <size of the array>;
+
+/*  creating an array of struct(eeprom_input) for taking in input */
+int *eeprom_input = new eeprom_struct(size_of_array);
+
+
+/* function definition for entering parameters of the fault coniditon
+passing the array of struct to the function */
+
+void eepromInit(int* eeprom_input, int& array_size)
+{
+    for (int i = 0; i < array_size; i++)
+    {
+    eeprom_input[i]->id = <id>;
+    eeprom_input[i]->size = <size>;
+
+    // calculating the address through the logic defined in shepherd
+    }
+}
+
 uint16_t eepromGetAddress(char *key)
 {
     /* find the index of the key in the eeprom_data array */
@@ -88,3 +120,4 @@ bool eeprom_write_data_index(uint16_t address, void *data, uint16_t size)
     /* write data to eeprom given page, offset, and size of data */
     eeprom_write(address, data, size);
 }
+
