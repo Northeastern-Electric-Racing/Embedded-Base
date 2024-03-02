082a43c2e0df5c38413c0245e8b50f1976daf737
nwdepatie <nwdepatie@gmail.com>
2024-03-02 03:43:12 -0500

    Moving CSynth along

--- cangen/CSynth.py
+++ cangen/CSynth.py
@@ -6,7 +6,7 @@ from typing import List
 """
 Example prototype
 
-void compute_send_mc_message(uint16_t user_max_charge, uint16_t user_max_discharge)
+void compute_send_mc_message(can_t *can, uint16_t user_max_charge, uint16_t user_max_discharge)
 {
 	struct __attribute__((packed)) {
 		uint16_t maxDischarge;
@@ -31,18 +31,17 @@ class CSynth:
 	"""
 
 	decode_data_import: str = (
-		'#include "can.h" \n#include <stdio.h>\n\n'  # Importing the Data struct and the FormatData and ProcessData traits
+		'#include "can.h" \n#include <stdio.h>\n\n'  # Including the necessary headers
 	)
 
 	decode_return_type: str = "void"  # The return type of any decode function
 	packed_struct_value: str = (
-		f"    struct __attribute__((packed)) {{"  # Initializing the result vector
+		f"    struct __attribute__((packed)) {{" # Initializing the packed strucct
 	)
 
 	packed_struct_end: str = "    } msg_data;\n"
 
-	decode_close: str = (
-		"    ]; \n    result\n}\n"  # Returning the result vector and closing the function
+	decode_close: str = ("    can_send_msg(&can, &mc_msg);\n}\n"  # Sending the CAN message
 	)
 
 	master_mapping_import: str = (
@@ -84,16 +83,20 @@ class CSynth:
 		signature: str = self.signature(msg.desc)
 		length_check: str = self.add_length_check(msg.networkEncoding[0].fields)
 		generated_lines: list[str] = []
-		print(self.packed_struct_end)
+
 		# Generate a line for each field in the message
 		generated_lines += self.parse_network_encoding(msg)
 		total_list: list[str] = (
 			[signature, self.packed_struct_value]
 			+ generated_lines
 			+ [self.packed_struct_end]
-			+ ["}\n"]
+			+ [self.format_msg(msg)]
+			+ [self.decode_close]
 		)
 		return "\n".join(total_list)
+
+	def format_msg (self, msg: CANMsg) -> str:
+		return f"    can_msg_t mc_msg;\n    mc_msg.id = {msg.id};\n    mc_msg.len = sizeof(mc_msg_data);"
 
 	def parse_network_encoding(
 		self, msg: CANMsg
@@ -109,8 +112,7 @@ class CSynth:
 			)
 		elif networkEncoding.id == "single_point":
 			for field in networkEncoding.fields:
-				result.append(f"             {self.decode_field_value(field)}")
-				result.append(f'        , "{field.name}", "{field.unit}"), ')
+				result.append(f"        {self.decode_field_value(field)} {field.name.replace('/', '_').lower()};\t/* {field.unit} */")
 		return result
 
 	def add_length_check(self, fields: List[CANField]) -> str:
@@ -126,7 +128,7 @@ class CSynth:
 
 	# Helper function that generates the signature of a decode function for a given CANMsg based off the can message description
 	def signature(self, desc: str) -> str:
-		return f"{self.decode_return_type} {self.function_name(desc)}(uint8_t data[], uint8_t len)\n{{"
+		return f"{self.decode_return_type} {self.function_name(desc)}(can_t *can, uint8_t data[], uint8_t len)\n{{"
 
 	# Helper function that generates a line the data struct for a given CANField value
 	def finalize_line(self, topic: str, unit: str, val: str) -> str:
@@ -143,8 +145,8 @@ class CSynth:
 			else:
 				base = f"&data[{field.index}..{field.index + field.size}]"
 			for decoder in field.decodings:
-				base = f"pd::{decoder.repr}({base} as {decoder.entry_type}, {decoder.bits})"
-			return f"{base} as {field.final_type}"
+				base = f"{decoder.entry_type}"
+			return f"{base}"
 
 	# Helper function that formats the data for a given CANField based off the format of the CANField if it exists, returns the decoded data otherwise
 	def format_data(self, field: CANField, decoded_data: str) -> str:
