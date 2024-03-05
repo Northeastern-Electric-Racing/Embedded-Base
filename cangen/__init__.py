bf0bfeb782dadda061fcbc73ec45dec1df769722
nwdepatie <nwdepatie@gmail.com>
2024-03-05 06:28:40 -0500

    Refactoring data structures representing CAN

--- cangen/__init__.py
+++ cangen/__init__.py
@@ -2,8 +2,6 @@ from cangen.RustSynth import RustSynth
 from cangen.CSynth import CSynth
 from cangen.YAMLParser import YAMLParser
 from cangen.CANField import CANField
-from cangen.Decoding import Decoding
 from cangen.Format import Format
 from cangen.Messages import Messages
-from cangen.NetworkEncoding import NetworkEncoding
 from cangen.Result import Result
