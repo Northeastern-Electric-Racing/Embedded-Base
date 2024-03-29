66b0b3fe286967131fad1ca3ffbebf7ed55a1c75
Nicholas DePatie <80368116+nwdepatie@users.noreply.github.com>
2024-03-28 20:19:04 -0400

    Implementing more concise implementation of CAN callbacks (#104)
    * Implementing more concise implementation of CAN callbacks
    
    * Actually getting it building in Cerberus with some mods
    
    * Removing unnecessary functions in favor of STM generated callbacks

---
+++ middleware/include/c_utils.h
@@ -0,0 +1,12 @@
+#ifndef C_UTILS
+#define C_UTILS
+
+/*
+ * Will retrieve the container of a certain pointer given the container type and its pointer
+ * Trick pulled from Linux Kernel
+ */
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
+#endif /* C_UTILS */
