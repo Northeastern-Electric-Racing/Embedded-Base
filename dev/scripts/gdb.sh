cebee5bcf6ca9e136f94716d7cb3c5a9185bd6ad
Jack Rubacha <rubacha.jack03@gmail.com>
2024-04-05 17:21:28 -0400

    initial dc

--- dev/scripts/gdb.sh
+++ dev/scripts/gdb.sh
@@ -3,6 +3,6 @@ elf_file=$(ls /home/app/build/*.elf)
 
 printf "$elf_file"
 
-openocd -f interface/cmsis-dap.cfg -f target/stm32f4x.cfg -c "adapter speed 5000" -c "init" -c "reset halt" & sleep 1 && arm-none-eabi-gdb $elf_file -ex "target remote localhost:3333"
+openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "init" -c "reset halt" & sleep 1 && arm-none-eabi-gdb $elf_file -ex "target remote localhost:3333"
 
 kill $(pidof openocd)
