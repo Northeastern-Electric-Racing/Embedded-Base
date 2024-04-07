e53cfef59b0790a3df7152e889449d68558e4458
root <root@DESKTOP-DEAD2D4>
2024-04-07 14:31:48 -0400

    #83 - cleanup code

--- general/include/ADS131M04.h
+++ general/include/ADS131M04.h
@@ -10,26 +10,19 @@ typedef struct
   GPIO_TypeDef *gpio;
   uint8_t cs_pin;
 
-} ads_config;
+} ads131_t;
 
-#define NULL_CMD 0x0
-#define RESET_CMD 0x11
-#define STANDBY_CMD 0x22
-#define WAKEUP_CMD 0x33
-#define LOCK_CMD 0x555
-#define UNLOCK_CMD 0x655
+/*  Method to initialize communication over SPI and configure the ADC into Continuous Conversion Mode*/
+void ads131m04_initialize(ltc_config *conf, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio, uint8_t cs_pin);
 
-// Method to initialize communication over SPI and
-void ADS131M04_initialize(ltc_config *conf, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio, uint8_t cs_pin);
+/* Method to abstract sending a command to SPI */
+void ads131m04_send_command(uint8_t cmd);
 
-// Method to abstract sending a command to SPI
-void ADS131M04_send_command(uint8_t cmd);
+/* Method to abstract writing to a register, will use SPI commands under the hood */
+void ads131m04_write_reg(uint8_t reg, uint8_t value);
 
-// Method to abstract writing to a register, will use SPI commands under the hood
-void ADS131M04_write_reg(uint8_t reg, uint8_t value);
+/* Method to abstract reading from a register, will use SPI commands under the hood to do */
+uint8_t ads131m04_read_reg(uint8_t reg);
 
-// Method to abstract reading from a register, will use SPI commands under the hood to do
-uint8_t ADS131M04_read_reg(uint8_t reg);
-
-// should read values out of the ADC
-uint8_t ADS131M04_read_ADC();
+/* Method to read values out of the ADC */
+uint8_t ads131m04_read_ADC();
--- general/src/ADS131M04.c
+++ general/src/ADS131M04.c
@@ -3,7 +3,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void ADS131M04_initialize(ads_config *conf, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio,
+#define NULL_CMD 0x0
+#define RESET_CMD 0x11
+#define STANDBY_CMD 0x22
+#define WAKEUP_CMD 0x33
+#define LOCK_CMD 0x555
+#define UNLOCK_CMD 0x655
+
+/*  Method to initialize communication over SPI and configure the ADC into Continuous Conversion Mode*/
+void ads131m04_initialize(ads131_t *conf, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio,
                           uint8_t cs_pin)
 {
     conf->spi = hspi;
@@ -14,30 +22,30 @@ void ADS131M04_initialize(ads_config *conf, SPI_HandleTypeDef *hspi, GPIO_TypeDe
 
     // Add code for configurations
 
-    ADS131M04_send_command(conf, RESET_CMD);
+    ads131m04_send_command(conf, RESET_CMD);
 
     return conf;
 }
 
-// Function to send command over SPI
-void ADS131M04_send_command(ads_config *config, uint8_t cmd)
+/* Method to abstract sending a command to SPI */
+void ads131m04_send_command(ads131_t *config, uint8_t cmd)
 {
     HAL_GPIO_WritePin(config->gpio, config->cs_pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(config->spi, cmd, 1, HAL_MAX_DELAY);
     HAL_GPIO_WritePin(config->gpio, config->cs_pin, GPIO_PIN_SET);
 }
 
-// Method to abstract writing to a register, will use SPI commands under the hood
-void ADS131M04_write_reg(uint8_t reg, uint8_t value)
+/* Method to abstract writing to a register, will use SPI commands under the hood */
+void ads131m04_write_reg(uint8_t reg, uint8_t value)
 {
 }
 
-// Method to abstract reading from a register, will use SPI commands under the hood to do
-uint8_t ADS131M04_read_reg(uint8_t reg)
+/* Method to abstract reading from a register, will use SPI commands under the hood to do */
+uint8_t ads131m04_read_reg(uint8_t reg)
 {
 }
 
-// should read values out of the ADC
-uint8_t ADS131M04_read_ADC()
+/* Method to read values out of the ADC */
+uint8_t ads131m04_read_ADC()
 {
 }
