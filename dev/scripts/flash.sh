bac1ad0193c4225f0cb2d182ef0738d429872e3e
Jack Rubacha <rubacha.jack03@gmail.com>
2024-04-09 13:49:26 -0400

    Docker compose ner-gcc-arm (#109)
    * Add build automation
    * Integrated with other repos

--- dev/scripts/flash.sh
+++ dev/scripts/flash.sh
@@ -3,4 +3,4 @@ elf_file=$(ls /home/app/build/*.elf)
 
 printf "$elf_file"
 
-openocd -f interface/cmsis-dap.cfg -f target/stm32f4x.cfg -c "adapter speed 5000" -c "program $elf_file verify reset exit"
+openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "program $elf_file verify reset exit"
--- dev/scripts/ody-connect.sh
+++ dev/scripts/ody-connect.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if [ $n -e /home/app/shepherd2.ioc ]
-then 
-    usbip attach -r 192.168.100.12 -b 1-1.4
-elif [ $n -e /home/app/cerberus.ioc ]
-then 
-    usbip attach -r 192.168.100.12 -b 1-1.3
-else
-    echo "No project found, to connect to usb port X at tpu IP P: usbip attach -r P -b 1-1.X"
-fi
+usbip attach -r 192.168.100.12 -b "$USPIP_PORT"
