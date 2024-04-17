983b7de9eebc120be8feb5504c8a32fd9284bf6d
Jack Rubacha <rubacha.jack03@gmail.com>
2024-04-17 17:05:27 -0400

    fix alias

--- dev/scripts/alias.sh
+++ dev/scripts/alias.sh
@@ -5,8 +5,9 @@ alias flash='/home/dev/scripts/flash.sh'
 alias emulate='/home/dev/scripts/emulate.sh'
 alias debug='/home/dev/scripts/gdb.sh'
 alias ody-connect='source /home/dev/scripts/ody-connect.sh'
-alias curr-serial='/home/dev/scripts/curr-serial.sh'
 
 function curr-serial() {
     udevadm info "$DEBUG_PORT" -a | sed -n -e 's/^.*ATTRS{serial}=="//p' | head -n 1 | sed 's/.$//'
 }
+
+export -f curr-serial
