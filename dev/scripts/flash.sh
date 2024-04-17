27f8a087fb851504b078971a282427b4dcfb7338
Jack Rubacha <rubacha.jack03@gmail.com>
2024-04-17 17:58:25 -0400

    fix serial cmsis

--- dev/scripts/flash.sh
+++ dev/scripts/flash.sh
@@ -3,4 +3,4 @@ elf_file=$(ls /home/app/build/*.elf)
 
 printf "$elf_file"
 
-openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "cmsis_dap_serial serial_string $(curr-serial)" -c "program $elf_file verify reset exit"
+openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "cmsis_dap_serial $(curr-serial)" -c "program $elf_file verify reset exit"
--- dev/scripts/gdb.sh
+++ dev/scripts/gdb.sh
@@ -3,6 +3,6 @@ elf_file=$(ls /home/app/build/*.elf)
 
 printf "$elf_file"
 
-openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "cmsis_dap_serial serial_string $(curr-serial)" -c "init" -c "reset halt" & sleep 1 && arm-none-eabi-gdb $elf_file -ex "target remote localhost:3333"
+openocd -f interface/cmsis-dap.cfg -f target/"$STM_TARGET_NAME"x.cfg -c "adapter speed 5000" -c "cmsis_dap_serial $(curr-serial)" -c "init" -c "reset halt" & sleep 1 && arm-none-eabi-gdb $elf_file -ex "target remote localhost:3333"
 
 kill $(pidof openocd)
