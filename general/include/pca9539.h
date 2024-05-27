30f864f6ed3cb283e66a58ff16e596e389e1f804
Jack Rubacha <rubacha.jack03@gmail.com>
2024-05-27 14:17:07 -0400

    make it way simpler and no bools

--- general/include/pca9539.h
+++ general/include/pca9539.h
@@ -5,6 +5,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/*
+PCA 9539 16 bit GPIO expander.  Datasheet: https://www.ti.com/lit/ds/symlink/pca9539.pdf?ts=1716785085909
+*/
+
 /// Possible I2C addresses, see comment below
 ///                            A0  A1
 ///    PCA_I2C_ADDR_0 0x74,    L   L
@@ -16,56 +20,43 @@
 #define PCA_I2C_ADDR_2 0x76
 #define PCA_I2C_ADDR_3 0x77
 
-/// @brief Pin #, (0 through 7 on each bus, ex. bus 1 pin 3 is 13)
-typedef enum {
-	PCA_P0,
-	PCA_P1,
-	PCA_P2,
-	PCA_P3,
-	PCA_P4,
-	PCA_P5,
-	PCA_P6,
-	PCA_P7,
-} pca9539_pins_t;
-
-/// @brief Bus #, Ex. P0 is B0 P0 and P12 is B1 P2
-typedef enum { PCA_B0, PCA_B1 } pca9539_port_t;
-
 /// What to write/read to/from
-///
+/// 0 is for pins 0X, 1 is for pins 1X
 /// INPUT: The incoming logic level (read only) Result: 1=H 0=L
 /// OUTPUT: The outgoing logic level (write only) Set: 1=H 0=L
 /// POLARITY: Inversion state, 1=Inverted 0=Uninverted
 /// DIRECTION: Input/Output selection 1=Input 0=Output
-typedef enum {
-	PCA_INPUT	  = 0,
-	PCA_OUTPUT	  = 2,
-	PCA_POLARITY  = 4,
-	PCA_DIRECTION = 6,
-} pca9539_reg_type_t;
 
-typedef struct {
-	I2C_HandleTypeDef* i2c_handle;
+#define PCA_INPUT_0 0x00
+#define PCA_INPUT_1 0x01
+#define PCA_OUTPUT_0 0x02
+#define PCA_OUTPUT_1 0x03
+#define PCA_POLARITY_0 0x04
+#define PCA_POLARITY_1 0x05
+#define PCA_DIRECTION_0 0x06
+#define PCA_DIRECTION_1 0x07
+
+typedef struct
+{
+	I2C_HandleTypeDef *i2c_handle;
 	uint16_t dev_addr;
 } pca9539_t;
 
 /// Init PCA9539, a 16 bit I2C GPIO expander
-void pca9539_init(pca9539_t* pca, I2C_HandleTypeDef* i2c_handle, uint8_t dev_addr);
+void pca9539_init(pca9539_t *pca, I2C_HandleTypeDef *i2c_handle, uint8_t dev_addr);
 
-/// @brief Read all pins on a bus, using reg_type input to get incoming logic level
-HAL_StatusTypeDef pca9539_read_pins(pca9539_t* pca, pca9539_reg_type_t reg_type,
-									pca9539_port_t port, bool config[8]);
+/// @brief Read all pins on a bus, for example using reg_type input to get incoming logic level
+HAL_StatusTypeDef pca9539_read_reg(pca9539_t *pca, uint8_t reg_type,
+									uint8_t config);
 /// @brief Read a specific pin on a bus, do not iterate over this, use read_pins instead
-HAL_StatusTypeDef pca9539_read_pin(pca9539_t* pca, pca9539_reg_type_t reg_type, pca9539_port_t port,
-								   pca9539_pins_t pin, bool* config);
+HAL_StatusTypeDef pca9539_read_pin(pca9539_t *pca, uint8_t reg_type,
+								   uint8_t pin, uint8_t *config);
 
-/// @brief Write all pins on a bus, using reg_type OUTPUT to set logic level or DIRECTION to set as
+/// @brief Write all pins on a bus, for example using reg_type OUTPUT to set logic level or DIRECTION to set as
 /// output
-HAL_StatusTypeDef pca9539_write_pins(pca9539_t* pca, pca9539_reg_type_t reg_type,
-									 pca9539_port_t port, bool config[8]);
+HAL_StatusTypeDef pca9539_write_reg(pca9539_t *pca, uint8_t reg_type, uint8_t config);
 /// @brief Write a specific pin on a bus, do not iterate over this, use write_pins instead
-HAL_StatusTypeDef pca9539_write_pin(pca9539_t* pca, pca9539_reg_type_t reg_type,
-									pca9539_port_t port, pca9539_pins_t pin,
-									bool config);
+HAL_StatusTypeDef pca9539_write_pin(pca9539_t *pca, uint8_t reg_type, uint8_t pin,
+									uint8_t config);
 
 #endif
--- general/src/pca9539.c
+++ general/src/pca9539.c
@@ -1,108 +1,69 @@
 #include "pca9539.h"
 
 #define REG_SIZE_BITS 8
-#define TIMEOUT HAL_MAX_DELAY
+#define TIMEOUT		  HAL_MAX_DELAY
 
-typedef enum
+HAL_StatusTypeDef pca_write_reg(pca9539_t* pca, uint16_t address, uint8_t* data)
 {
-    PCA9539_INPUT = 0,
-    PCA9539_OUTPUT = 2,
-    PCA9539_POLARITY = 4,
-    PCA9539_DIRECTION = 6,
-} register_type_t;
-
-HAL_StatusTypeDef pca_write_reg(pca9539_t *pca, uint16_t address, pca9539_port_t port, uint8_t *data)
-{
-    if (port == PCA_B1)
-    {
-        address = address + 1;
-    }
-    // ensure shifting left one, HAL adds the write bit
-    return HAL_I2C_Mem_Write(pca->i2c_handle, pca->dev_addr << 1, address, I2C_MEMADD_SIZE_8BIT, data, sizeof(data), TIMEOUT);
+	// ensure shifting left one, HAL adds the write bit
+	return HAL_I2C_Mem_Write(pca->i2c_handle, pca->dev_addr << 1, address, I2C_MEMADD_SIZE_8BIT,
+							 data, sizeof(data), TIMEOUT);
 }
 
-HAL_StatusTypeDef pca_read_reg(pca9539_t *pca, uint16_t address, pca9539_port_t port, uint8_t *data)
+HAL_StatusTypeDef pca_read_reg(pca9539_t* pca, uint16_t address, uint8_t* data)
 {
-    if (port == PCA_B1)
-    {
-        address = address + 1;
-    }
 
-    return HAL_I2C_Mem_Read(pca->i2c_handle, pca->dev_addr << 1, address, I2C_MEMADD_SIZE_8BIT, data, sizeof(data), TIMEOUT);
+	return HAL_I2C_Mem_Read(pca->i2c_handle, pca->dev_addr << 1, address, I2C_MEMADD_SIZE_8BIT,
+							data, sizeof(data), TIMEOUT);
 }
 
-static uint8_t create_buf(bool config[8])
+void pca9539_init(pca9539_t* pca, I2C_HandleTypeDef* i2c_handle, uint8_t dev_addr)
 {
-    uint8_t data = 0;
-    for (uint8_t i = 0; i < 8; i++)
-    {
-        data |= config[i] << i;
-    }
-    return data;
+	pca->i2c_handle = i2c_handle;
+	pca->dev_addr	= dev_addr << 1u; /* shifted one to the left cuz STM says so */
 }
 
-static void deconstruct_buf(uint8_t *data, bool *config)
+HAL_StatusTypeDef pca9539_read_reg(pca9539_t* pca, uint8_t reg_type, uint8_t config)
 {
-    for (uint8_t i = 0; i < 8; i++)
-    {
-        config[i] = (*data >> i) & 1;
-    }
+
+	HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, &config);
+	if (status) {
+		return status;
+	}
+
+	return status;
 }
 
-void pca9539_init(pca9539_t *pca, I2C_HandleTypeDef *i2c_handle, uint8_t dev_addr)
+HAL_StatusTypeDef pca9539_read_pin(pca9539_t* pca, uint8_t reg_type, uint8_t pin, uint8_t* config)
 {
-    pca->i2c_handle = i2c_handle;
-    pca->dev_addr = dev_addr << 1u; /* shifted one to the left cuz STM says so */
+	uint8_t data;
+	HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, &data);
+	if (status) {
+		return status;
+	}
+
+	*config = (data & (1 << pin)) > 0;
+
+	return status;
 }
 
-HAL_StatusTypeDef pca9539_read_pins(pca9539_t *pca, pca9539_reg_type_t reg_type, pca9539_port_t port, bool config[8])
+HAL_StatusTypeDef pca9539_write_reg(pca9539_t* pca, uint8_t reg_type, uint8_t config)
 {
 
-    uint8_t data = create_buf(config);
-    HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, port, &data);
-    if (status)
-    {
-        return status;
-    }
-
-    deconstruct_buf(&data, config);
-
-    return status;
+	return pca_write_reg(pca, reg_type, &config);
 }
 
-HAL_StatusTypeDef pca9539_read_pin(pca9539_t *pca, pca9539_reg_type_t reg_type, pca9539_port_t port, pca9539_pins_t pin, bool *config)
-{
-    uint8_t data;
-    HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, port, &data);
-    if (status)
-    {
-        return status;
-    }
-
-    *config = (data & (0b1 << pin)) > 0;
-
-    return status;
-}
-
-HAL_StatusTypeDef pca9539_write_pins(pca9539_t *pca, pca9539_reg_type_t reg_type, pca9539_port_t port, bool config[8])
+HAL_StatusTypeDef pca9539_write_pin(pca9539_t* pca, uint8_t reg_type, uint8_t pin, uint8_t config)
 {
 
-    uint8_t buf = create_buf(config);
-    return pca_write_reg(pca, reg_type, port, &buf);
-}
+	uint8_t data;
 
-HAL_StatusTypeDef pca9539_write_pin(pca9539_t *pca, pca9539_reg_type_t reg_type, pca9539_port_t port, pca9539_pins_t pin, bool config)
-{
+	HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, &data);
+	if (status) {
+		return status;
+	}
 
-    uint8_t data;
+	uint8_t data_new = (data & ~(1u << pin)) | (config << pin);
 
-    HAL_StatusTypeDef status = pca_read_reg(pca, reg_type, port, &data);
-    if (status)
-    {
-        return status;
-    }
-
-    uint8_t data_new = (data & ~(1u << pin)) | (config << pin);
-
-    return pca_write_reg(pca, reg_type, port, &data_new);
+	return pca_write_reg(pca, reg_type, &data_new);
 }
