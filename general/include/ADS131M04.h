bd77e755742de5986e320e682252c125d5ee5aee
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2024-06-04 17:20:25 -0400

    #83 - ADS131M04 Driver (#108)
    * #83 - basic interface and basic spi
    
    * #83 - add reset command
    
    * #83 - interface
    
    * #83 - cleanup code
    
    * #83 - implement readreg, writereg, and readadc
    
    * #83 - update interface
    
    * #83 - make interface more abstract, read correct data amount from SPI
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-DEAD2D4> (This is Reid lol)
    Co-authored-by: nwdepatie <nwdepatie@gmail.com>

---
+++ general/include/ADS131M04.h
@@ -0,0 +1,16 @@
+#ifndef ADS131M04_H
+#define ADS131M04_H
+
+#include "stm32xx_hal.h"
+
+typedef struct {
+  SPI_HandleTypeDef *spi;
+  GPIO_TypeDef *gpio;
+  uint8_t cs_pin;
+} ads131_t;
+
+/*  Method to initialize communication over SPI and configure the ADC into Continuous Conversion Mode*/
+void ads131m04_init(ads131_t *adc, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio, uint8_t cs_pin);
+
+/* Method to read values out of the ADC */
+HAL_StatusTypeDef ads131m04_read_adc(ads131_t *adc);
