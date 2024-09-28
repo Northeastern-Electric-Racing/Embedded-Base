1f4a19eb48c28182e03b73a95aace39aadc53da7
harry <harrisonceckert@icloud.com>
2024-09-28 19:07:23 -0400

    Added handling for instances of EncodableCANMsg

--- cangen/__init__.py
+++ cangen/__init__.py
@@ -1,5 +1,7 @@
 from cangen.RustSynth import RustSynth
 from cangen.YAMLParser import YAMLParser
+from cangen.CANMsg import CANMsg
+from cangen.CANMsg import EncodableCANMsg
 from cangen.CANField import NetField
 from cangen.Format import Format
 from cangen.Messages import Messages
