7bb56c89ac4e131753353a087af204ddc06329d8
Dylan Donahue <donahue.dyl@northeastern.edu>
2024-10-06 17:32:08 -0400

    fixed container of

--- middleware/include/c_utils.h
+++ middleware/include/c_utils.h
@@ -5,15 +5,12 @@
  * Will retrieve the container of a certain pointer given the container type and its pointer
  * Trick pulled from Linux Kernel
  */
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
+#define CONTAINER_OF(ptr, type, member) \
+	((type *)((char *)(ptr) - offsetof(type, member)))
 
 #endif /* C_UTILS */
 
-
 void endian_swap(void *ptr, int size);
-
 
 /// @brief Reverses the bit order of a byte
 /// @param b byte
