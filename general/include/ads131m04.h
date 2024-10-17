d3153711ec52df2723685570821c728f5feb6738
bjackson312006 <blakejackson312006@gmail.com>
2024-10-17 12:46:44 -0400

    Removed HAL-specific code in ads131m04 drver and added function pointers

--- general/include/ads131m04.h
+++ general/include/ads131m04.h
@@ -3,18 +3,25 @@
 
 #include "stm32xx_hal.h"
 
+/* Function Pointers*/
+typedef void (*GPIO_WriteFuncPtr)(uint16_t cs_pin, uint8_t PinState);
+typedef void (*SPI_TransmitFuncPtr)(uint8_t *pData, uint16_t size);
+typedef int (*SPI_ReceiveFuncPtr)(uint8_t *pData, uint16_t size);
+
 typedef struct {
-	SPI_HandleTypeDef *spi;
-	GPIO_TypeDef *gpio;
-	uint8_t cs_pin;
+	uint16_t cs_pin;
+	GPIO_WriteFuncPtr local_GPIO_Write;
+	SPI_TransmitFuncPtr local_SPI_Transmit;
+	SPI_ReceiveFuncPtr local_SPI_Receive;
 } ads131_t;
 
 /*  Method to initialize communication over SPI and configure the ADC into
  * Continuous Conversion Mode*/
-void ads131m04_init(ads131_t *adc, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio,
-		    uint8_t cs_pin);
+void ads131m04_init(ads131_t *adc, GPIO_WriteFuncPtr local_GPIO_write,
+		    SPI_TransmitFuncPtr local_SPI_transmit,
+		    SPI_ReceiveFuncPtr local_SPI_receive);
 
 /* Method to read values out of the ADC */
-HAL_StatusTypeDef ads131m04_read_adc(ads131_t *adc, uint32_t *adc_values);
+int ads131m04_read_adc(ads131_t *adc, uint32_t *adc_values);
 
 #endif /* ADS131M04_H */
