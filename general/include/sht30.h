8ac8973e2bcfce5b0d6ca9051f104038ca25020b
marvin-g-neu <141779828+marvin-g-neu@users.noreply.github.com>
2024-11-03 14:58:52 -0500

    working version of sht

--- general/include/sht30.h
+++ general/include/sht30.h
@@ -1,28 +1,34 @@
-#ifndef SHT30_H
-#define SHT30_H
+#ifndef sht30_h
+#define sht30_h
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stddef.h>
 
-/**A
+/**
  * https://www.mouser.com/datasheet/2/682/Sensirion_Humidity_Sensors_SHT3x_Datasheet_digital-971521.pdf
  * --Datasheet
  *
- */                                   
-#define SHT30_I2C_ADDR (0x44u << 1u) /* If ADDR (pin2) is connected to VDD, 0x45 */
+ */
+#define SHT30_I2C_ADDR \
+	0x44 << 1u /* If ADDR (pin2) is connected to VDD, 0x45  \
+                                   */
 
 typedef enum {
-  SHT3X_COMMAND_MEASURE_HIGHREP_STRETCH = 0x2c06u,
-  SHT3X_COMMAND_CLEAR_STATUS = 0x3041u,
-  SHT3X_COMMAND_SOFT_RESET = 0x30A2u,
-  SHT3X_COMMAND_HEATER_ENABLE = 0x306du,
-  SHT3X_COMMAND_HEATER_DISABLE = 0x3066u,
-  SHT3X_COMMAND_READ_STATUS = 0xf32du,
-  SHT3X_COMMAND_FETCH_DATA = 0xe000u,
-  SHT3X_COMMAND_MEASURE_HIGHREP_10HZ = 0x2737u,
-  SHT3X_COMMAND_MEASURE_LOWREP_10HZ = 0x272au
+	SHT3X_COMMAND_MEASURE_HIGHREP_STRETCH = 0x2c06,
+	SHT3X_COMMAND_CLEAR_STATUS = 0x3041,
+	SHT3X_COMMAND_SOFT_RESET = 0x30A2,
+	SHT3X_COMMAND_HEATER_ENABLE = 0x306d,
+	SHT3X_COMMAND_HEATER_DISABLE = 0x3066,
+	SHT3X_COMMAND_READ_STATUS = 0xf32d,
+	SHT3X_COMMAND_FETCH_DATA = 0xe000,
+	SHT3X_COMMAND_MEASURE_HIGHREP_10HZ = 0x2737,
+	SHT3X_COMMAND_MEASURE_LOWREP_10HZ = 0x272a
 } sht3x_command_t;
+
+/** Function Pointers */
+typedef int (*Write_ptr)(uint8_t *data, uint8_t reg, uint8_t length);
+typedef int (*Read_ptr)(uint8_t *data, uint8_t reg, uint8_t length);
+typedef void (*DelayFunc)(uint32_t ms);
 
 /*
  * Start measurement command with clock streching enabled and high
@@ -35,36 +41,29 @@ typedef enum {
  * repeatability */
 #define SHT30_START_CMD_NCS 0x2400
 
-// Function pointer types
-typedef int (*Write_ptr)(uint8_t addr, const uint8_t *data, size_t len, uint32_t timeout);
-typedef int (*Read_Ptr)(uint8_t addr, uint8_t *data, size_t len, uint32_t timeout);
-typedef int (*Mem_Read_Ptr)(uint8_t addr, uint16_t mem_addr, uint16_t mem_addr_size, uint8_t *data, size_t len, uint32_t timeout);
-typedef void (*DelayFunc)(uint32_t ms);
-
-
 typedef struct {
-  Write_ptr write;
-  Read_Ptr read;
-  Mem_Read_Ptr mem_read;
-  DelayFunc delay;
-  uint16_t temp;
-  uint16_t humidity;
+	Write_ptr write_reg;
+	Read_ptr read_reg;
+	DelayFunc delay;
+	uint16_t temp;
+	uint16_t humidity;
+	bool is_heater_enabled;
 } sht30_t;
 
 /**
  * @brief Initializes an SHT30 Driver
  *
  * @param sht30 - SHT30 driver
- * @return int - 0 on success, negative value on error
+ * @return int - Status code
  */
-int sht30_init(sht30_t *sht30);
+int sht30_init(sht30_t *sht30, Write_ptr write_reg, Read_ptr read_reg, DelayFunc delay);
 
 /**
  * @brief Toggles the status of the internal heater
  *
  * @param sht30 - SHT30 driver
  * @param enable - true to enable, false to disable
- * @return int - 0 on success, negative value on error
+ * @return int - Status code
  */
 int sht30_toggle_heater(sht30_t *sht30, bool enable);
 
@@ -72,8 +71,8 @@ int sht30_toggle_heater(sht30_t *sht30, bool enable);
  * @brief Retrieves the temperature and humidity
  *
  * @param sht30 - SHT30 driver
- * @return int - 0 on success, negative value on error
+ * @return int - Status code
  */
 int sht30_get_temp_humid(sht30_t *sht30);
 
-#endif 
+#endif
--- general/src/sht30.c
+++ general/src/sht30.c
@@ -1,92 +1,109 @@
 #include "sht30.h"
+#include <stdbool.h>
+#include <stdio.h>
 
-static int sht30_write_reg(const sht30_t *sht30, uint16_t command) {
-  uint8_t command_buffer[2] = {(uint8_t)((command & 0xff00u) >> 8u), (uint8_t)(command & 0xffu)};
-  return sht30->i2c_transmit(SHT30_I2C_ADDR, command_buffer, sizeof(command_buffer), 30u);
+static int sht30_write_reg(sht30_t *sht30, uint16_t command)
+{
+	uint8_t command_buffer[2] = { (command & 0xff00u) >> 8u,
+				      command & 0xffu };
+
+	return sht30->write_reg(command_buffer, 0, sizeof(command_buffer));
 }
 
 /**
  * @brief Calculates the CRC by using the polynomial x^8 + x^5 + x^4 + 1
  * @param data: the data to use to calculate the CRC
  */
-static uint8_t calculate_crc(const uint8_t *data, size_t length) {
-  uint8_t crc = 0xffu;
-  for (size_t i = 0u; i < length; i++) {
-    crc ^= data[i];
-    for (uint8_t j = 0u; j < 8u; j++) {
-      if ((crc & 0x80u) != 0u) {
-        crc = (uint8_t)((uint8_t)(crc << 1u) ^ 0x31u);
-      } else {
-        crc = (uint8_t)(crc << 1u);
-      }
-    }
-  }
-  return crc;
+static uint8_t calculate_crc(const uint8_t *data, size_t length)
+{
+	uint8_t crc = 0xff;
+	for (size_t i = 0; i < length; i++) {
+		crc ^= data[i];
+		for (size_t j = 0; j < 8; j++) {
+			if ((crc & 0x80u) != 0) {
+				crc = (uint8_t)((uint8_t)(crc << 1u) ^ 0x31u);
+			} else {
+				crc <<= 1u;
+			}
+		}
+	}
+	return crc;
 }
 
-static uint16_t uint8_to_uint16(uint8_t msb, uint8_t lsb) {
-  return (uint16_t)((uint16_t)msb << 8u) | lsb;
+static uint16_t uint8_to_uint16(uint8_t msb, uint8_t lsb)
+{
+	return (uint16_t)((uint16_t)msb << 8u) | lsb;
 }
 
-int sht30_init(sht30_t *sht30) {
-  uint8_t status_reg_and_checksum[3];
-  int status = sht30->i2c_mem_read(SHT30_I2C_ADDR, SHT3X_COMMAND_READ_STATUS, 2u,
-                                   status_reg_and_checksum, sizeof(status_reg_and_checksum), 30u);
-  if (status != 0) {
-    return status;
-  }
+int sht30_init(sht30_t *sht30, Write_ptr write_reg, Read_ptr read_reg, DelayFunc delay)
+{
+	sht30->write_reg = write_reg;
+	sht30->read_reg = read_reg;
+	sht30->delay = delay;
 
-  uint8_t calculated_crc = calculate_crc(status_reg_and_checksum, 2u);
-  if (calculated_crc != status_reg_and_checksum[2]) {
-    return -1;
-  }
+	uint8_t status_reg_and_checksum[3];
+	if (sht30->read_reg(status_reg_and_checksum, SHT3X_COMMAND_READ_STATUS,
+			     sizeof(status_reg_and_checksum)) != 0) {
+		return -1;	
+	}
 
-  return 0;
+	uint8_t calculated_crc = calculate_crc(status_reg_and_checksum, 2);
+
+	if (calculated_crc != status_reg_and_checksum[2]) {
+		return -1;
+	}
+
+	return 0;
 }
 
-int sht30_toggle_heater(sht30_t *sht30, bool enable) {
-    if (enable) {
-        return sht30_write_reg(sht30, SHT3X_COMMAND_HEATER_ENABLE);
-    } else {
-        return sht30_write_reg(sht30, SHT3X_COMMAND_HEATER_DISABLE);
-    }
+int sht30_toggle_heater(sht30_t *sht30, bool enable)
+{
+	if (enable) {
+		return sht30_write_reg(sht30, SHT3X_COMMAND_HEATER_ENABLE);
+	} else {
+		return sht30_write_reg(sht30, SHT3X_COMMAND_HEATER_DISABLE);
+	}
 }
 
-int sht30_get_temp_humid(sht30_t *sht30) {
-  union {
-    struct __attribute__((packed)) {
-      uint16_t temp;
-      uint8_t temp_crc;
-      uint16_t humidity;
-      uint8_t humidity_crc;
-    } raw_data;
-    uint8_t databuf[6];
-  } data;
+int sht30_get_temp_humid(sht30_t *sht30)
+{
+	union {
+		struct __attribute__((packed)) {
+			uint16_t temp; // The packed attribute does not correctly arrange the bytes
+			uint8_t temp_crc;
+			uint16_t humidity; // The packed attribute does not correctly arrange the bytes
+			uint8_t humidity_crc;
+		} raw_data;
+		uint8_t databuf[6];
+	} data;
 
-  int status = sht30_write_reg(sht30, SHT30_START_CMD_WCS);
-  if (status != 0) {
-    return status;
-  }
+	uint16_t temp, humidity;
 
-  status = sht30->i2c_receive(SHT30_I2C_ADDR, data.databuf, sizeof(data.databuf), 30u);
-  if (status != 0) {
-    return status;
-  }
+	sht30_write_reg(sht30, (SHT30_START_CMD_WCS));
 
-  uint16_t temp = uint8_to_uint16(data.databuf[0], data.databuf[1]);
-  if (data.raw_data.temp_crc != calculate_crc(data.databuf, 2u)) {
-    return -1;
-  }
+	sht30->delay(1);
 
-  sht30->temp = (uint16_t)(temp_val / 1000u);
+	if (sht30->read_reg(data.databuf, 0, sizeof(data.databuf)) != 0) {
+		return -1;
+	}
 
-  uint16_t humidity = uint8_to_uint16(data.databuf[3], data.databuf[4]);
-  if (data.raw_data.humidity_crc != calculate_crc(data.databuf + 3, 2u)) {
-    return -1;
-  }
+	temp = uint8_to_uint16(data.databuf[0], data.databuf[1]);
 
-  humidity = (uint16_t)(100.0f * (float)humidity / 65535.0f);
-  sht30->humidity = humidity;
+	if (data.raw_data.temp_crc != calculate_crc(data.databuf, 2)) {
+		return -1;
+	}
 
-  return 0;
+	float val = -45.0f + 175.0f * (float)temp / 65535.0f;
+
+	sht30->temp = (uint16_t)val;
+
+	humidity = uint8_to_uint16(data.databuf[3], data.databuf[4]);
+	if (data.raw_data.humidity_crc != calculate_crc(data.databuf + 3, 2)) {
+		return -1;
+	}
+
+	humidity = (uint16_t)(100.0f * (float)humidity / 65535.0f);
+	sht30->humidity = humidity;
+
+	return 0;
 }
