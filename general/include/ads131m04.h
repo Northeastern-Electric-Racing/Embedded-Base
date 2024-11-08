7ab9dd0c583773193bfb56752f649ef0f89ee05e
Blake Jackson <blakejackson312006@gmail.com>
2024-11-08 15:41:46 -0500

    Make ads131m driver agnostic (#182)
    * Removed HAL-specific code in ads131m04 drver and added function pointers
    
    * Fixed cs_pin data type in ads131m04 (changed from uint16_t to uint8_t)
    
    * Variable and function pointer name changes

--- general/include/ads131m04.h
+++ general/include/ads131m04.h
@@ -3,18 +3,24 @@
 
 #include "stm32xx_hal.h"
 
+/* Function Pointers*/
+typedef void (*GPIO_WritePtr)(uint8_t pin, uint8_t PinState);
+typedef void (*WritePtr)(uint8_t *pData, uint16_t size);
+typedef int (*ReadPtr)(uint8_t *pData, uint16_t size);
+
 typedef struct {
-	SPI_HandleTypeDef *spi;
-	GPIO_TypeDef *gpio;
-	uint8_t cs_pin;
+	uint8_t pin;
+	GPIO_WritePtr gpio_write;
+	WritePtr write;
+	ReadPtr read;
 } ads131_t;
 
 /*  Method to initialize communication over SPI and configure the ADC into
  * Continuous Conversion Mode*/
-void ads131m04_init(ads131_t *adc, SPI_HandleTypeDef *hspi, GPIO_TypeDef *hgpio,
-		    uint8_t cs_pin);
+void ads131m04_init(ads131_t *adc, GPIO_WritePtr gpio_write, WritePtr write,
+		    ReadPtr read);
 
 /* Method to read values out of the ADC */
-HAL_StatusTypeDef ads131m04_read_adc(ads131_t *adc, uint32_t *adc_values);
+int ads131m04_read_adc(ads131_t *adc, uint32_t *adc_values);
 
 #endif /* ADS131M04_H */
