31779496bd5453b719b3229b344ae52137f65005
DimitryP6 <111515533+DimitryP6@users.noreply.github.com>
2025-01-09 20:08:10 +0300

    Delete middleware/simple_ema.c

--- middleware/simple_ema.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// Exponential Moving Average Lowpass Filter
-void ema_filter(float current_value, float *previous_ema, float alpha)
-{
-	*previous_ema =
-		(alpha * current_value) + ((1 - alpha) * (*previous_ema));
-}
