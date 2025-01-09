32ab3c2b3baa2a1b075e6e563ea031b20d7129b4
DimitryP6 <dimitryolegpianykh@gmail.com>
2025-01-08 22:28:43 -0500

    added void to function line

--- middleware/simple_ema.h
+++ middleware/simple_ema.h
@@ -2,6 +2,6 @@
 #define SIMPLE_EMA
 
 // Exponential Moving Average Lowpass Filter
-ema_filter(float current_value, float *previous_ema, float alpha);
+void ema_filter(float current_value, float *previous_ema, float alpha);
 
 #endif /* SIMPLE_EMA */
