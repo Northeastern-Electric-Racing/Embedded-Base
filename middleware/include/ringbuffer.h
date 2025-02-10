43603a9953c2773cc9646d533e821f4b08d1b155
Kaushik Mehta <mehta.kau@northeastern.edu>
2025-02-10 15:33:47 -0500

    Updated ring buffer implementation.

--- middleware/include/ringbuffer.h
+++ middleware/include/ringbuffer.h
@@ -3,7 +3,7 @@
  * @brief A ringbuffer implementation on the stack.
  *
  * @copyright Copyright Northeastern Electric Racing 2025. All rights reserved.
- * 
+ *
  */
 
 #ifndef RINGBUFFER_H
@@ -11,20 +11,28 @@
 
 #include <stddef.h>
 
-typedef struct ringbuf_t ringbuf_t;
+typedef struct ringbuf_t {
+  void **buf;
+  size_t size; /* Maximum number of elements that can be in the ringbuffer */
+  size_t head_idx;
+  size_t curr_elements; /* Number of elements in the RB */
+  size_t element_size;
+} ringbuf_t;
 
 /**
  * @brief Initialize a ringbuffer.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @param buffer Pointer to stack allocated buffer.
  * @param size The number of elements in the ringbuffer.
+ * @param element_size The size of each element in bytes.
  */
-void rb_init(struct ringbuf_t *rb, void **buffer, size_t size);
+void rb_init(struct ringbuf_t *rb, void *buffer, size_t size,
+             size_t element_size);
 
 /**
  * @brief Get the last element to be inserted in the ringbuffer.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @param ptr Pointer to read data to.
  */
@@ -32,7 +40,7 @@ void *rb_get_head(const struct ringbuf_t *rb);
 
 /**
  * @brief Pop the head off of the RB.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @return void* Data at the head of the RB.
  */
@@ -40,7 +48,7 @@ void *rb_dequeue(struct ringbuf_t *rb);
 
 /**
  * @brief Get the element at the tail of the ringbuffer.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @param ptr Pointer to read data to.
  */
@@ -48,7 +56,7 @@ void *rb_get_tail(const struct ringbuf_t *rb);
 
 /**
  * @brief Get the last N elements from the head of the ringbuffer.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @param buf Buffer that data will be written to.
  * @param n Number of elements, starting at the head, to read into the buffer.
@@ -58,12 +66,10 @@ void rb_get_last_n(const struct ringbuf_t *rb, void *buf, size_t n);
 /**
  * @brief Insert an element at the head of the ringbuffer. Overwrites data at
  * the tail if the RB is full.
- * 
+ *
  * @param rb Pointer to ringbuffer.
  * @param data The data to insert at the head of the ringbuffer.
  */
-void rb_insert(const struct ringbuf_t *rb, void *data);
-
-
+void rb_insert(struct ringbuf_t *rb, void *data);
 
 #endif
--- middleware/src/ringbuffer.c
+++ middleware/src/ringbuffer.c
@@ -3,98 +3,92 @@
  * @brief A ringbuffer implementation on the stack.
  *
  * @copyright Copyright Northeastern Electric Racing 2025. All rights reserved.
- * 
+ *
  */
 
 #include "ringbuffer.h"
 
 #include <assert.h>
+#include <stdint.h>
+#include <string.h>
 
-struct ringbuf_t {
-	void **buf;
-	size_t size; /* Maximum number of elements that can be in the ringbuffer */
-	size_t head_idx;
-	size_t curr_elements; /* Number of elements in the RB */
-};
+static void *get_nth_element(const struct ringbuf_t *rb, size_t n) {
+  assert(rb);
+  assert(n < rb->curr_elements);
 
-/**
- * @brief Get the nth element from the head. Checks that you are getting 
- * a valid value with asserts. n=0 gets the head of the RB.
- * 
- * @param rb Pointer to ringbuffer.
- * @param n nth element to get.
- * @return void* The value at that location
- */
-static void *get_nth_element(const struct ringbuf_t *rb, size_t n)
-{
-	assert(rb);
-	assert(n < rb->size);
-	assert(rb->curr_elements > ((rb->head_idx - n) % rb->size));
+  size_t index = (rb->head_idx + rb->size - rb->curr_elements + n) % rb->size;
 
-	return rb->buf[(rb->head_idx - n) % rb->size];
+  return (uint8_t *)rb->buf + (index * rb->element_size);
 }
 
-void rb_init(struct ringbuf_t *rb, void **buffer, size_t size)
-{
-	rb->buf = buffer;
-	rb->size = size;
-	rb->head_idx = 0;
+void rb_init(struct ringbuf_t *rb, void **buffer, size_t size) {
+  rb->buf = buffer;
+  rb->size = size;
+  rb->element_size = element_size;
+  rb->head_idx = 0;
+  rb->curr_elements = 0;
 }
 
-void *rb_get_head(const struct ringbuf_t *rb)
-{
-	assert(rb);
-	assert(rb->curr_elements);
+void *rb_get_head(const struct ringbuf_t *rb) {
+  assert(rb);
+  assert(rb->curr_elements > 0);
 
-	return rb->buf[rb->head_idx];
+  size_t last_entry_idx = (rb->head_idx + rb->size - 1) % rb->size;
+
+  return (uint8_t *)rb->buf + (last_entry_idx * rb->element_size);
 }
 
-void *rb_dequeue(struct ringbuf_t *rb)
-{
-	void *ptr = rb_get_head(rb);
+void *rb_dequeue(struct ringbuf_t *rb) {
+  assert(rb);
 
-	if (rb->head_idx == 0 && rb->curr_elements == 1) {
-		rb->head_idx = 0;
-	} else {
-		rb->head_idx -= 1;
-		rb->curr_elements -= 1;
-	}
+  if (rb->curr_elements == 0) {
+    return NULL;
+  }
 
-	return ptr;
+  size_t tail_idx = (rb->head_idx + rb->size - rb->curr_elements) % rb->size;
+  void *ptr = (uint8_t *)rb->buf + (tail_idx * rb->element_size);
+
+  rb->curr_elements--;
+
+  return ptr;
 }
 
-void *rb_get_tail(const struct ringbuf_t *rb)
-{
-	assert(rb);
+void *rb_get_tail(const struct ringbuf_t *rb) {
+  assert(rb);
+  assert(rb->curr_elements > 0);
 
-	void *ptr;
-
-	/*  
-     * If the RB has not wrapped around yet, then the 
-     * tail is at 0 
-     */
-	if (rb->curr_elements < rb->size) {
-		ptr = rb->buf[0];
-	} else {
-		ptr = get_nth_element(rb, rb->head_idx + 1);
-	}
-
-	return ptr;
+  size_t tail_idx = (rb->head_idx + rb->size - rb->curr_elements) % rb->size;
+  return (uint8_t *)rb->buf + (tail_idx * rb->element_size);
 }
 
-void rb_get_last_n(const struct ringbuf_t *rb, void *buf, size_t n)
-{
-	assert(rb);
-	assert(buf);
+void rb_get_last_n(const struct ringbuf_t *rb, void *buf, size_t n) {
+  assert(rb);
+  assert(buf);
 
-	for (size_t i = 0; i < n; i++) {
-		rb->buf[i] = get_nth_element(rb, i);
-	}
+  if (n > rb->curr_elements) {
+    n = rb->curr_elements;
+  }
+
+  size_t start_idx = rb->curr_elements - n;
+
+  for (size_t i = 0; i < n; i++) {
+    void *src = get_nth_element(rb, start_idx + i);
+    void *dest = (uint8_t *)buf + (i * rb->element_size);
+    memcpy(dest, src, rb->element_size);
+  }
 }
 
-void rb_insert(const struct ringbuf_t *rb, void *data)
-{
-	assert(rb);
-	assert(data);
-	rb->buf[(rb->head_idx + 1) % rb->size] = data;
+void rb_insert(struct ringbuf_t *rb, void *data) {
+  assert(rb);
+  assert(data);
+
+  void *dest = (uint8_t *)rb->buf + (rb->head_idx * rb->element_size);
+
+  memcpy(dest, data, rb->element_size);
+
+  rb->head_idx = (rb->head_idx + 1) % rb->size;
+
+  if (rb->curr_elements < rb->size) {
+    rb->curr_elements++;
+  }
 }
