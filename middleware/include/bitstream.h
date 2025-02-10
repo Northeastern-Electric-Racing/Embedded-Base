42330806750c8a3879c5d8dd884cc24469f1cddb
bjackson312006 <blakejackson312006@gmail.com>
2025-02-10 11:53:16 -0500

    Changed implementation

--- middleware/include/bitstream.h
+++ middleware/include/bitstream.h
@@ -4,55 +4,38 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include "c_utils.h"
 
 typedef struct {
 	uint8_t *data; // The bitstream data, stored as bytes
-	size_t length; // The length of the bitstream in bits
-	size_t last_bit; // The current last bit in the bitstream
+	size_t bytes; // The number of bytes in the bitstream
+	size_t total_bits; // Total number of bits in the bitstream
 } bitstream_t;
 
 /**
  * @brief Initializes a bitstream with the specified length.
  * @param {bitstream_t*} The bitstream to initialize.
- * @param {size_t} The length of the bitstream in bits.
- * @return Returns 1 if failed, and 0 if successful.
+ * @param {uint8_t*} The buffer to store the bitstream data.
+ * @param {size_t} The length of the bitstream in bytes.
  */
-int bitstream_init(bitstream_t *bitstream, size_t length);
+void bitstream_init(bitstream_t *bitstream, uint8_t *data, size_t bytes);
 
 /**
  * @brief Adds data to a bitstream. The data is added to the end of the bitstream.
  * @param {bitstream_t*} The bitstream to add data to.
- * @param {void*} The data to add to the bitstream.
+ * @param {uint32_t} The data to add to the bitstream.
  * @param {size_t} The length of the data in bits.
  * @return Returns 1 if failed, and 0 if successful.
  */
-int bitstream_add(bitstream_t *bitstream, void *data, size_t input_length);
+int bitstream_add(bitstream_t *bitstream, uint32_t value, size_t num_bits);
 
 /**
- * @brief Gets a pointer to the last byte in the bitstream.
- * @param {bitstream_t*} The bitstream to get the head of.
- * @return Returns a pointer to the last byte in the bitstream.
- */
-uint8_t *bitstream_get_head(bitstream_t *bitstream);
-
-/**
- * @brief Reads a range of bits from a bitstream. The minimum index is 0, and the maximum index is bitstream->last_bit.
+ * @brief Reads info from the bitstream.
  * @param {bitstream_t*} The bitstream to read from.
- * @param {size_t} The index of the first bit in the range you want to read. (Inclusive)
- * @param {size_t} The index of the last bit in the range you want to read. (Inclusive)
- * @param {void*} The output buffer to write the bits to.
- * @param {size_t} The size of the output buffer in bits (uint8_t is 8 bits, uint16_t is 16 bits, etc.).
+ * @param {uint32_t} The bit to start reading from.
+ * @param {size_t} The amount of bits to read. The maximum is 32 bits, and (start_bit + num_bits) must be less than the total number of bits in the bitstream.
  * @return Returns 1 if failed, and 0 if successful.
  */
-int bitstream_read(bitstream_t *bitstream, size_t start_index, size_t end_index,
-		   void *output, size_t output_length);
-
-/** 
- * @brief Frees the bitstream's data array, and resets the bitstream's length and last_bit.
- * @param {bitstream_t*} The bitstream to destroy.
- * @return Returns 1 if failed, and 0 if successful.
- */
-int bitstream_destroy(bitstream_t *bitstream);
+uint32_t bitstream_read(bitstream_t *bitstream, uint32_t start_bit,
+			size_t num_bits);
 
 #endif // BITSTREAM_H
--- middleware/src/bitstream.c
+++ middleware/src/bitstream.c
@@ -1,96 +1,44 @@
 #include "bitstream.h"
 
-int bitstream_init(bitstream_t *bitstream, size_t length)
+void bitstream_init(bitstream_t *bitstream, uint8_t *data, size_t bytes)
 {
-	bitstream->length = length;
-	bitstream->last_bit = 0;
-
-	if (!bitstream) {
-		return 1;
-	}
-
-	bitstream->data = (uint8_t *)calloc(
-		(length + 7) / 8,
-		sizeof(uint8_t)); // Allocate memory for the bitstream data, and initialize all bits to 0.
-
-	if (!bitstream->data) {
-		free(bitstream->data);
-		return 1; // If memory allocation fails, return 1.
-	}
-
-	return 0;
+	bitstream->data = data;
+	bitstream->total_bits = 0;
+	bitstream->bytes = bytes;
+	memset(bitstream->data, 0, bytes); // Zero out the buffer
 }
 
-int bitstream_add(bitstream_t *bitstream, void *data, size_t input_length)
+int bitstream_add(bitstream_t *bitstream, uint32_t value, size_t num_bits)
 {
-	uint8_t *input_data = (uint8_t *)data;
-	input_data[input_length / 8] <<=
-		8 -
-		(input_length %
-		 8); // Shift the last byte to the left to remove the unused bits.
-	for (int i = 0; i < input_length; i++) {
-		uint8_t input_bit = NER_GET_BIT(
-			input_data[i / 8],
-			7 - (i % 8)); // Extract the bit from the input data.
+	if (bitstream->total_bits + num_bits > (bitstream->bytes * 8)) {
+		return -1; // Error: not enough space in the bitstream
+	}
 
-		if (input_bit) {
-			NER_SET_BIT(bitstream->data[bitstream->last_bit / 8],
-				    7 - (bitstream->last_bit %
-					 8)); // Set the bit in the bitstream.
-		} else {
-			NER_CLEAR_BIT(
-				bitstream->data[bitstream->last_bit / 8],
-				7 - (bitstream->last_bit %
-				     8)); // Clear the bit in the bitstream.
+	for (int i = 0; i < num_bits; ++i) {
+		if (value & (1 << (num_bits - 1 - i))) {
+			bitstream->data[(bitstream->total_bits + i) / 8] |=
+				(1 << (7 - ((bitstream->total_bits + i) % 8)));
 		}
-		bitstream->last_bit++; // Increment the index of the last bit.
 	}
-	return 0;
+	bitstream->total_bits += num_bits;
+	return 0; // Success
 }
 
-uint8_t *bitstream_get_head(bitstream_t *bitstream)
+uint32_t bitstream_read(bitstream_t *bitstream, uint32_t start_bit,
+			size_t num_bits)
 {
-	return bitstream->data +
-	       (bitstream->last_bit /
-		8); // Return a pointer to the last byte in the bitstream.
-}
+	uint32_t result = 0;
 
-int bitstream_destroy(bitstream_t *bitstream)
-{
-	if (!bitstream || !bitstream->data) {
-		return 1;
+	if (start_bit + num_bits > bitstream->total_bits) {
+		return -1; // Error: trying to read beyond bit length
 	}
-	free(bitstream->data);
-	bitstream->data = NULL;
-	bitstream->length = 0;
-	bitstream->last_bit = 0;
-	return 0;
-}
 
-int bitstream_read(bitstream_t *bitstream, size_t start_index, size_t end_index,
-		   void *input_buffer, size_t input_length)
-{
-	uint8_t *input = (uint8_t *)input_buffer;
-	size_t input_bit_index = 0;
-	for (size_t i = start_index; i <= end_index; i++) {
-		/* Extract bit from bitstream */
-		uint8_t byte_index = i / 8;
-		uint8_t bit_position = 7 - (i % 8);
-		uint8_t bit =
-			NER_GET_BIT(bitstream->data[byte_index], bit_position);
-
-		if (bit) {
-			NER_SET_BIT(input[input_bit_index / 8],
-				    7 - (input_bit_index % 8));
-		} else {
-			NER_CLEAR_BIT(input[input_bit_index / 8],
-				      7 - (input_bit_index % 8));
+	for (int i = 0; i < num_bits; ++i) {
+		if (bitstream->data[(start_bit + i) / 8] &
+		    (1 << (7 - ((start_bit + i) % 8)))) {
+			result |= (1 << (num_bits - 1 - i));
 		}
-		input_bit_index++;
 	}
-	input[input_bit_index / 8] >>=
-		8 -
-		(input_bit_index %
-		 8); // Shift the last byte to the right to remove the unused bits.
-	return 0;
+
+	return result;
 }
