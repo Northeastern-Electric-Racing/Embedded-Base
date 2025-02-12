ab2abee4e1dedd99ae3e0684e0774156499c532e
bjackson312006 <blakejackson312006@gmail.com>
2025-02-11 21:38:28 -0500

    Changed comment format

--- middleware/include/bitstream.h
+++ middleware/include/bitstream.h
@@ -13,26 +13,26 @@ typedef struct {
 
 /**
  * @brief Initializes a bitstream with the specified length.
- * @param {bitstream_t*} The bitstream to initialize.
- * @param {uint8_t*} The buffer to store the bitstream data.
- * @param {size_t} The length of the bitstream in bytes.
+ * @param *bitstream The bitstream to initialize.
+ * @param *data The buffer to store the bitstream data.
+ * @param bytes The length of the bitstream in bytes.
  */
 void bitstream_init(bitstream_t *bitstream, uint8_t *data, size_t bytes);
 
 /**
  * @brief Adds data to a bitstream. The data is added to the end of the bitstream.
- * @param {bitstream_t*} The bitstream to add data to.
- * @param {uint32_t} The data to add to the bitstream.
- * @param {size_t} The length of the data in bits.
+ * @param *bitstream The bitstream to add data to.
+ * @param value The data to add to the bitstream.
+ * @param num_bits The length of the data in bits.
  * @return Returns 1 if failed, and 0 if successful.
  */
 int bitstream_add(bitstream_t *bitstream, uint32_t value, size_t num_bits);
 
 /**
  * @brief Reads info from the bitstream.
- * @param {bitstream_t*} The bitstream to read from.
- * @param {uint32_t} The bit to start reading from.
- * @param {size_t} The amount of bits to read. The maximum is 32 bits, and (start_bit + num_bits) must be less than the total number of bits in the bitstream.
+ * @param *bitstream The bitstream to read from.
+ * @param start_bit The bit to start reading from.
+ * @param num_bits The amount of bits to read. The maximum is 32 bits, and (start_bit + num_bits) must be less than the total number of bits in the bitstream.
  * @return Returns 1 if failed, and 0 if successful.
  */
 uint32_t bitstream_read(bitstream_t *bitstream, uint32_t start_bit,
