d4c8f84e221967268b6d43e1b3866e43fd6f5db4
Scott A <89099102+Sabramz@users.noreply.github.com>
2025-02-11 19:47:03 -0500

    243: Add docs for can_add_filter

--- platforms/stm32f405/include/can.h
+++ platforms/stm32f405/include/can.h
@@ -24,7 +24,18 @@ typedef struct {
 } can_msg_t;
 
 HAL_StatusTypeDef can_init(can_t *can);
+
+/**
+ * @brief Add CANIDs to the CAN whitelist filter. Up to 7 additions of 4
+ * IDs can be done. Only supports CAN standard IDs.
+ * 
+ * @param can CAN datastruct.
+ * @param id_list List of CAN IDs to whitelist. Must be an array of four
+ * elements.
+ * @return HAL_StatusTypeDef Error code.
+ */
 HAL_StatusTypeDef can_add_filter(can_t *can, uint32_t *id_list);
+
 HAL_StatusTypeDef can_send_msg(can_t *can, can_msg_t *msg);
 HAL_StatusTypeDef can_send_extended_msg(can_t *can, can_msg_t *msg);
 
