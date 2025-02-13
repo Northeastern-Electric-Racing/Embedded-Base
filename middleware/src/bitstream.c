fdcbbf78ee9763ad485d7fa8a17bc1979e79d5d9
Blake Jackson <blakejackson312006@gmail.com>
2025-02-12 20:45:07 -0500

    Bitstream Datastructure (#241)

---
+++ middleware/src/bitstream.c
@@ -0,0 +1,44 @@
+#include "bitstream.h"
+
+void bitstream_init(bitstream_t *bitstream, uint8_t *data, size_t bytes)
+{
+	bitstream->data = data;
+	bitstream->total_bits = 0;
+	bitstream->bytes = bytes;
+	memset(bitstream->data, 0, bytes); // Zero out the buffer
+}
+
+int bitstream_add(bitstream_t *bitstream, uint32_t value, size_t num_bits)
+{
+	if (bitstream->total_bits + num_bits > (bitstream->bytes * 8)) {
+		return -1; // Error: not enough space in the bitstream
+	}
+
+	for (int i = 0; i < num_bits; ++i) {
+		if (value & (1 << (num_bits - 1 - i))) {
+			bitstream->data[(bitstream->total_bits + i) / 8] |=
+				(1 << (7 - ((bitstream->total_bits + i) % 8)));
+		}
+	}
+	bitstream->total_bits += num_bits;
+	return 0; // Success
+}
+
+uint32_t bitstream_read(bitstream_t *bitstream, uint32_t start_bit,
+			size_t num_bits)
+{
+	uint32_t result = 0;
+
+	if (start_bit + num_bits > bitstream->total_bits) {
+		return -1; // Error: trying to read beyond bit length
+	}
+
+	for (int i = 0; i < num_bits; ++i) {
+		if (bitstream->data[(start_bit + i) / 8] &
+		    (1 << (7 - ((start_bit + i) % 8)))) {
+			result |= (1 << (num_bits - 1 - i));
+		}
+	}
+
+	return result;
+}
