dd74d33abd643eaef41a91c30213dd61966cf7f3
Peyton-McKee <mckee.p@northeastern.edu>
2025-02-26 23:27:03 -0500

    Make ADC and DMA STM work

--- dev/renode/peripherals/PCA9539.cs
+++ dev/renode/peripherals/PCA9539.cs
@@ -5,8 +5,7 @@ using Antmicro.Renode.Core.Structure.Registers;
 using Antmicro.Renode.Utilities;
 using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.GPIOPort;
-using Antmicro.Renode.Peripherals.Bus;
-using Antmicro.Renode.Utilities.Collections;
+using System.Linq;
 
 namespace Antmicro.Renode.Peripherals.I2C
 {
@@ -14,21 +13,23 @@ namespace Antmicro.Renode.Peripherals.I2C
     {
         public PCA9539(Machine machine) : base(machine, NumberGPIOs)
         {
-            IRQ = new GPIO();
-            irqManager = new GPIOInterruptManager(IRQ, State);
             registers = DefineRegisters();
         }
 
         public byte[] Read(int count = 1)
         {
-            return BitConverter.GetBytes(registers.Read((long)context));
+            this.Log(LogLevel.Info, "READING FROM PCA: {0}", context);
+            byte[] bytes = BitConverter.GetBytes((short)registers.Read((long)context));
+            this.Log(LogLevel.Info, "RETURNING: {0}", string.Join(", ", bytes));
+            return bytes;
         }
 
         public void Write(byte[] data)
         {
+            this.Log(LogLevel.Info, "WRITING TO PCA: {0}", string.Join(", ", data));
             context = (Registers)data[0];
-            if ((int)context < 0x2) return; // Indicates that a read operation to input ports is next, but no changes are necessary
-            registers.Write((long)context, data[0]);
+            if ((int)context < 0x2 || data.Length < 2) return; // Indicates that a read operation to input ports is next, but no changes are necessary
+            registers.Write((long)context, data[1]);
         }
 
         private ByteRegisterCollection DefineRegisters()
@@ -36,90 +37,85 @@ namespace Antmicro.Renode.Peripherals.I2C
             var registersMap = new Dictionary<long, ByteRegister>
             {
                 {
-                    (long)(Registers.InputPort0), new ByteRegister(this)
-                                        .WithValueField(0, 8, name: "INPUT0", FieldMode.Read, valueProviderCallback: _ =>
+                    (long)Registers.InputPort0, new ByteRegister(this)
+                                        .WithValueField(0, 8, FieldMode.Read, name: "INPUT0", valueProviderCallback: _ =>
                                         {
-                                            var gpioPins = irqManager.PinDirection.Select(x => (x & GPIOInterruptManager.Direction.Input) != 0);
-                                            var result = gpioPins.Zip(State, (pins, state) => pin && state);
-                                            return BitHelper.getValueFromBitsArray(result);
+                                            var result = new Span<bool>(State, 0, 8).ToArray();
+                                            return BitHelper.GetValueFromBitsArray(result);
                                         })
                 },
                 {
-                    (long)(Registers.InputPort1), new ByteRegister(this)
-                                        .WithValueField(0, 8, name: $"INPUT1", FieldMode.Read, valueProviderCallback: _ =>
+                    (long)Registers.InputPort1, new ByteRegister(this)
+                                        .WithValueField(0, 8, FieldMode.Read, name: $"INPUT1", valueProviderCallback: _ =>
                                         {
-                                            var gpioPins = irqManager.PinDirection.Select(x => (x & GPIOInterruptManager.Direction.Input) != 0);
-                                            var result = gpioPins.Zip(State, (pins, state) => pin && state);
-                                            return BitHelper.getValueFromBitsArray(result);
+                                            var result = new Span<bool>(State, 7, 8).ToArray();
+                                            return BitHelper.GetValueFromBitsArray(result);
                                         })
                 },
                 {
-                    (long)(Registers.OutputPort0),
+                    (long)Registers.OutputPort0,
                     new ByteRegister(this)
-                        .WithValueField(0, 8, name: $"OUPUT0", valueProviderCallback: val => {
-                            var gpioPins = irqManager.PinDirection.Select(x => (x & GPIOInterruptManager.Direction.Output) != 0);
-                            var result = gpioPins.Zip(Connection.Values, (pin, state) => pin && state.IsSet);
-                            return BitHelper.GetValueFromBitsArrray(result);
-                        }, writeCallback: (_, val) =>
-                        {
-                            var bits = BitHelper.GetBits(val);
-                            for (var i = 0; i < bits.length; i++) {
-                                if ((irqManager.PinDirection[i] & GPIOInterruptManager.Direction.Output) != 0) {
-                                    Connections[i].Set(bits[i]);
-                                }
-                            }
-                            BitHelper.ReplaceBits(ref outputValue, width: 8, source: val, destinationPosition: 16);
-                        })
+                        .WithValueField(0, 8, name: $"OUPUT0", writeCallback: (_, val) => WriteCallback((byte) val))
                 },
                 {
-                    (long)(Registers.OutputPort1),
+                    (long)Registers.OutputPort1,
                     new ByteRegister(this)
-                        .WithValueField(0, 8, name: $"OUPUT1", valueProviderCallback: val => {
-                            var gpioPins = irqManager.PinDirection.Select(x => (x & GPIOInterruptManager.Direction.Output) != 0);
-                            var result = gpioPins.Zip(Connection.Values, (pin, state) => pin && state.IsSet);
-                            return BitHelper.GetValueFromBitsArrray(result);
-                        }, writeCallback: (_, val) =>
+                        .WithValueField(0, 8, name: $"OUPUT1", writeCallback: (_, val) => WriteCallback((byte) val, 8))
+                },
+                {
+                    (long)Registers.PolarityInversionPort0,
+                    new ByteRegister(this).WithValueField(0, 8, name: $"POLARITY0", writeCallback: (_, val) =>
                         {
-                            var bits = BitHelper.GetBits(val);
-                            for (var i = 0; i < bits.length; i++) {
-                                if ((irqManager.PinDirection[i] & GPIOInterruptManager.Direction.Output) != 0) {
-                                    Connections[i].Set(bits[i]);
-                                }
-                            }
-                            BitHelper.ReplaceBits(ref outputValue, width: 8, source: val, destinationPosition: 16 - 8);
+                            BitHelper.ReplaceBits(ref polarityInversion, width: 8, source: (uint)val);
                         })
                 },
                 {
-                    (long)(Registers.PolarityInversionPort0),
-                    new ByteRegister(this).WithFieldValue(0, 8, name: $"POLARITY0", writeCallback: (_, val) =>
+                    (long)Registers.PolarityInversionPort1,
+                    new ByteRegister(this).WithValueField(0, 8, name: $"POLARITY1", writeCallback: (_, val) =>
                         {
-                            BitHelper.ReplaceBits(ref polarityInversion, width: 8, source: val, destinationPosition: 16);
+                            BitHelper.ReplaceBits(ref polarityInversion, width: 8, source: (uint)val, destinationPosition: 8);
                         })
                 },
                 {
-                    (long)(Registers.PolarityInversionPort1),
-                    new ByteRegister(this).WithFieldValue(0, 8, name: $"POLARITY1", writeCallback: (_, val) =>
-                        {
-                            BitHelper.ReplaceBits(ref polarityInversion, width: 8, source: val, destinationPosition: 16 - 8);
-                        })
+                    (long)Registers.ConfigurationPort0,
+                    new ByteRegister(this).WithValueField(0, 8, name: $"CONFIGURATION0", writeCallback: (_, val) => ConfigurationCallback((byte) val))
                 },
                 {
-                    (long)(Registers.ConfigurationPort0),
-                    new ByteRegister(this).WithFieldValue(0, 8, name: $"CONFIGURATION0", writeCallback: (_, val) =>
-                        {
-                            BitHelper.ReplaceBits(ref configuration, width: 8, source: val, destinationPosition: 16);
-                        })
-                },
-                {
-                    (long)(Registers.ConfigurationPort1),
-                    new ByteRegister(this).WithFieldValue(0, 8, name: $"CONFIGURATION1", writeCallback: (_, val) =>
-                        {
-                            BitHelper.ReplaceBits(ref configuration, width: 8, source: val, destinationPosition: 16 - 8);
-                        })
+                    (long)Registers.ConfigurationPort1,
+                    new ByteRegister(this).WithValueField(0, 8, name: $"CONFIGURATION1", writeCallback: (_, val) => ConfigurationCallback((byte) val, 8))
                 }
             };
 
             return new ByteRegisterCollection(this, registersMap);
+        }
+
+        private void WriteCallback (byte value, int offset = 0) {
+            BitHelper.ReplaceBits(ref outputFlipFlop, width: 8, source: (byte)value, destinationPosition: offset);
+            this.SetPins();
+            this.Log(LogLevel.Info, "Writing {0}: {1} to {2}", offset, OutputFlipFlop, value);
+        }
+
+        private void ConfigurationCallback(byte value, int offset = 0) {
+            BitHelper.ReplaceBits(ref configuration, width: 8, source: (byte)value, destinationPosition: offset);
+            this.SetPins();
+            this.Log(LogLevel.Info, "Configuring {0}: {1} to {2}", offset, Configuration, value);
+        }
+
+        private void SetPins() {
+            var outputBits = new Span<bool>(BitHelper.GetBits(outputFlipFlop), 0, 16).ToArray();
+            var configurationBits = new Span<bool>(BitHelper.GetBits(configuration), 0, 16).ToArray();
+
+            for (var i = 0; i < outputBits.Length; i++) {
+                if (!configurationBits[i]) { // False means that it is enabled as output
+                    WritePin(i, outputBits[i]);
+                }
+            }
+        }
+        private void WritePin(int number, bool value)
+        {
+            this.Log(LogLevel.Info, "Setting {0} to {1}", number, value);
+            State[number] = value;
+            Connections[number].Set(value);
         }
 
         public void FinishTransmission()
@@ -130,14 +126,14 @@ namespace Antmicro.Renode.Peripherals.I2C
         public GPIO IRQ { get; private set; }
 
         private const int NumberGPIOs = 16;
-        public uint SubregistersCount = 2;
-        private uint inputValue;
-        private uint outputValue = 0xFFFF;
-        private uint polarityInversion = 0x0000;
+        private uint polarityInversion = 0xFFFF;
         private uint configuration = 0xFFFF;
+        private uint outputFlipFlop = 0xFFFF;
+        public uint PolarityInversion { get => polarityInversion; }
+        public uint Configuration { get => configuration; }
+        public uint OutputFlipFlop { get => outputFlipFlop; }
         private readonly ByteRegisterCollection registers;
         private Registers context;
-        private readonly GPIOInterruptManager irqManager;
 
         private enum Registers : byte
         {
