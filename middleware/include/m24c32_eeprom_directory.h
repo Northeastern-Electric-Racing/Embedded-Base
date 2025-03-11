abce49f4c3769db3cc984308ac5eba760bd3e0e6
Kaushik Mehta <mehta.kau@northeastern.edu>
2025-03-11 15:05:16 -0400

    Refactor: m24c32 EEPROM directory read and write functions.

--- middleware/include/m24c32_eeprom_directory.h
+++ middleware/include/m24c32_eeprom_directory.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <m24c32.h>
-#include <eepromdirectory.h>
+#include "m24c32.h"
+#include "eepromdirectory.h"
 
 /**
  * @brief Write to a section of the EEPROM. Writes that overflow the size of the partition
@@ -12,11 +12,11 @@
  * @param data Data to write.
  * @param len Length of data to write in Bytes.
  * @param offset Offset from partition base address in Bytes.
- * @return int Error code. -1 if id is not a valid partition ID, -2 if attempting to write
+ * @return int Error code. -1 if id is not a valid partition ID, -2 if offset is invalid, -3 if attempting to write
  * into the next partition.
  */
-int m24c32_directory_write(const struct eeprom_directory *directory,
-			   m24c32_t *eeprom, char *id, uint8_t *data,
+int m24c32_directory_write(const eeprom_directory_t *directory,
+			   m24c32_t *eeprom, const char *id, uint8_t *data,
 			   size_t len, size_t offset);
 
 /**
@@ -28,8 +28,9 @@ int m24c32_directory_write(const struct eeprom_directory *directory,
  * @param data Read output.
  * @param len Length of data to write in Bytes.
  * @param offset Offset from partition base address in Bytes.
- * @return int Error code.
+ * @return int Error code. -1 if id is not a valid partition ID, -2 if offset is invalid, -3 if attempting to read
+ * into the next partition.
  */
-int m24c32_directory_read(const struct eeprom_directory *directory,
-			  m24c32_t *eeprom, char *id, uint8_t data, size_t len,
+int m24c32_directory_read(const eeprom_directory_t *directory, m24c32_t *eeprom,
+			  const char *id, uint8_t *data, size_t len,
 			  size_t offset);
--- middleware/src/m24c32_eeprom_directory.c
+++ middleware/src/m24c32_eeprom_directory.c
@@ -1,33 +1,46 @@
-#include <m24c32_eeprom_directory.h>
+#include "m24c32_eeprom_directory.h"
 
-int m24c32_directory_write(const struct eeprom_directory *directory,
-			   m24c32_t *eeprom, char *id, uint8_t *data,
+int m24c32_directory_write(const eeprom_directory_t *directory,
+			   m24c32_t *eeprom, const char *id, uint8_t *data,
 			   size_t len, size_t offset)
 {
 	int32_t base = eeprom_get_base_address(directory, id);
 	if (base < 0) {
-		return -1;
+		return -1; // Invalid ID
 	}
 
 	int32_t size = eeprom_get_size(directory, id);
 
-	/* Overwriting into next partition */
-	if (base + offset + len > base + size) {
-		return -2;
+	if (offset >= size) {
+		return -2; // Invalid Offset
+	}
+
+	if (offset + len > size) {
+		return -3; // Overwrting next partition
 	}
 
 	return m24c32_write(eeprom, base + offset, data, len);
 }
 
-int m24c32_directory_read(m24c32_t *eeprom, char *id, uint8_t data, size_t len,
+int m24c32_directory_read(const eeprom_directory_t *directory, m24c32_t *eeprom,
+			  const char *id, uint8_t *data, size_t len,
 			  size_t offset)
 {
 	int32_t base = eeprom_get_base_address(directory, id);
+
 	if (base < 0) {
-		return -1;
+		return -1; // Invalid ID
 	}
 
 	int32_t size = eeprom_get_size(directory, id);
 
-	return m24c32_write(eeprom, base + offset, data, len);
+	if (offset >= size) {
+		return -2; // Invalid Offset
+	}
+
+	if (offset + len > size) {
+		return -3; // Reading beyond current partition
+	}
+
+	return m24c32_read(eeprom, base + offset, data, len);
 }
