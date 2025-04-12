ccd88c0595e7814cc5f4a0508150d1f908ef2ed1
bjackson312006 <blakejackson312006@gmail.com>
2025-04-12 12:15:11 -0400

    Updated comments

--- middleware/include/serial.h
+++ middleware/include/serial.h
@@ -5,7 +5,7 @@
 * @brief Adds a new data point to a serial monitor window.
 * @param title The title of the serial monitor window.
 * @param data_label The label for the data point.
-* @param format The datatype format string for the data point. (e.g. "%d" "%f" "%s" etc.)
+* @param format The datatype format for the data point. (e.g. "%d" "%f" "%s" etc.)
 * @param ... The data point to be added. This can be a number, string, etc. The datatype must match the format string.
 *
 * @note Example usage: serial_monitor("Test Panel", "Test Val", "%d", test_val);
@@ -17,8 +17,8 @@ void serial_monitor(const char *title, const char *data_label,
 * @brief Adds a new data point to a serial graph window.
 * @param title The title of the serial graph window.
 * @param data_label The label for the data point.
-* @param format The datatype format string for the data point. (e.g. "%d" "%f" "%s" etc.)
-* @param ... The data point to be added. This must be a plottable value (e.g. a number). The datatype must match the format string.
+* @param format The datatype format for the data point. (e.g. "%d" "%f" "%u" etc.). This MUST be a plotable value (i.e. a number).
+* @param ... The data point to be added. The datatype must match the format string.
 *
 * @note Example usage: serial_graph("Test Graph", "Test Val", "%d", test_val);
 */
