2a5670180e6b882c52fc95ad58ca9d9c8cbcf4a7
Jake Hensley <150979806+Jake-Hensley@users.noreply.github.com>
2025-04-14 19:59:21 -0400

    Sht30 (#237)
    Co-authored-by: Jack Rubacha <rubacha.jack03@gmail.com>
    Co-authored-by: Caio <caiodasilva05@outlook.com>
    Co-authored-by: Caio DaSilva <145620095+caiodasilva2005@users.noreply.github.com>

--- general/include/sht30.h
+++ general/include/sht30.h
@@ -1,19 +1,14 @@
 #ifndef sht30_h
 #define sht30_h
-
-#include "stm32xx_hal.h"
 #include <stdbool.h>
 #include <stdint.h>
-
 /**
  * https://www.mouser.com/datasheet/2/682/Sensirion_Humidity_Sensors_SHT3x_Datasheet_digital-971521.pdf
  * --Datasheet
  *
  */
 #define SHT30_I2C_ADDR \
-	0x44 << 1u /* If ADDR (pin2) is connected to VDD, 0x45  \
-                                   */
-
+	0x44 /* If ADDR (pin2) is connected to VDD, 0x45  This is shifted left as it is required to be read as a dev address.*/
 typedef enum {
 	SHT3X_COMMAND_MEASURE_HIGHREP_STRETCH = 0x2c06,
 	SHT3X_COMMAND_CLEAR_STATUS = 0x3041,
@@ -25,49 +20,49 @@ typedef enum {
 	SHT3X_COMMAND_MEASURE_HIGHREP_10HZ = 0x2737,
 	SHT3X_COMMAND_MEASURE_LOWREP_10HZ = 0x272a
 } sht3x_command_t;
-
+/** Function Pointers */
+typedef int (*Write_ptr)(uint8_t *data, uint8_t dev_address, uint8_t length);
+typedef int (*Read_ptr)(uint8_t *data, uint16_t command, uint8_t dev_address,
+			uint8_t length);
 /*
  * Start measurement command with clock streching enabled and high
  * repeatability. This is responsible for retrieving the temp and humidity in
  * single shot mode
  */
 #define SHT30_START_CMD_WCS 0x2C06
-
 /* Start measurement command with clock streching disabled and high
  * repeatability */
 #define SHT30_START_CMD_NCS 0x2400
-
 typedef struct {
-	I2C_HandleTypeDef *i2c_handle;
-	uint16_t status_reg;
-	uint16_t temp;
-	uint16_t humidity;
+	Write_ptr write_reg;
+	Read_ptr read_reg;
+	Read_ptr blocking_read_reg;
+	float temp;
+	float humidity;
 	bool is_heater_enabled;
+	uint8_t dev_address;
 } sht30_t;
-
 /**
  * @brief Initializes an SHT30 Driver
  *
  * @param sht30 - SHT30 driver
- * @return HAL_StatusTypeDef
+ * @return int - Status code
  */
-HAL_StatusTypeDef sht30_init(sht30_t *sht30);
-
+uint8_t sht30_init(sht30_t *sht30, Write_ptr write_reg, Read_ptr read_reg,
+		   Read_ptr blocking_read_reg, uint8_t dev_address);
 /**
  * @brief Toggles the status of the internal heater
  *
  * @param sht30 - SHT30 driver
  * @param enable - true to enable, false to disable
- * @return HAL_StatusTypeDef
+ * @return int - Status code
  */
-HAL_StatusTypeDef sht30_toggle_heater(sht30_t *sht30, bool enable);
-
+int sht30_toggle_heater(sht30_t *sht30, bool enable);
 /**
  * @brief Retrieves the temperature and humidity
  *
  * @param sht30 - SHT30 driver
- * @return HAL_StatusTypeDef
+ * @return int - Status code
  */
-HAL_StatusTypeDef sht30_get_temp_humid(sht30_t *sht30);
-
+uint8_t sht30_get_temp_humid(sht30_t *sht30);
 #endif
