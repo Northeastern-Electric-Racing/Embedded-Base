1e9ab95c84b956a94d56031608761d333c575860
Caio <caiodasilva05@outlook.com>
2025-05-06 13:01:58 -0400

    trying to compile

--- dev/scripts/mock.sh
+++ dev/scripts/mock.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
-MOCKED_HEADERS=(
-    "../../../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os.h"
-    "../platforms/stm32f405/include/can.h"
-    "../../../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h"
-)
+INCLUDE_PATHS=()
+
+for arg in "$@"; do
+    if [[ "$arg" == -I* ]]; then
+        INCLUDE_PATHS+=("${arg:2}")
+    fi
+done
 
 MOCK_DIR="./mocks"
 mkdir -p $MOCK_DIR
 
-for header in "${MOCKED_HEADERS[@]}"; do
-    ruby /cmock_portable/lib/cmock.rb "$header"
+for include_path in "${INCLUDE_PATHS[@]}"; do
+    while IFS= read -r -d '' header; do
+        base=$(basename "$header")
+        [[ "$base" == *stm* ]] && continue
+        [[ "$base" == *cmsis_os* ]] && continue
+        ruby /cmock_portable/lib/cmock.rb "$header"
+    done < <(find "$include_path" -type f -name '*.h' -print0)
 done
 
-for mock in $MOCKS/Mock*.h; do
-    base=$(basename $mock | sed 's/^Mock//')
-    ln -sf $mock $MOCKS/$base
-done
+
