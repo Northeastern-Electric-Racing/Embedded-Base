6025368f19f9ece8378684cb942e824df661bad8
Caio <caiodasilva05@outlook.com>
2025-05-21 20:54:48 -0400

    compiling all

--- dev/scripts/mock.sh
+++ dev/scripts/mock.sh
@@ -5,7 +5,6 @@ MOCK_DIR="./mocks"
 mkdir -p $MOCK_DIR
 
 for header in "$@"; do
-    echo "Generating mock for $header"
     ruby /cmock_portable/lib/cmock.rb -ocmock_config.yml "$header"
 done
     
