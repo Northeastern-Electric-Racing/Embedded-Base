f7aa02824db131fa0c186c32c33fc5e62f734fa6
Caio <caiodasilva05@outlook.com>
2025-05-25 22:05:14 -0400

    supporting test packages

--- dev/scripts/mock.sh
+++ dev/scripts/mock.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+MOCK_OUTPUT_DIR="$1"
+
+shift
+
 for header in "$@"; do
-    ruby /cmock_portable/lib/cmock.rb -ocmock_config.yml "$header"
+    ruby /cmock_portable/lib/cmock.rb -ocmock_config.yml --mock_path="$MOCK_OUTPUT_DIR" "$header"
 done
     
 
