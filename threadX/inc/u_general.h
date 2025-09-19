dc017627aa866eac0e9be4181b626818d8428871
Caio <caiodasilva05@outlook.com>
2025-09-18 20:33:18 -0400

    moving thread helpers

---
+++ threadX/inc/u_general.h
@@ -0,0 +1,14 @@
+
+#ifndef __U_GENERAL_H
+#define __U_GENERAL_H
+
+#include <string.h>
+#include <stdio.h>
+/* This file (and u_general.c) contain miscellaneous macros, functions, and configuration settings that can be used throughout the project. */
+
+/* General-purpose status macros. */
+#define U_SUCCESS     0
+#define U_ERROR       1
+#define U_QUEUE_EMPTY 2
+
+#endif
---
+++ threadX/inc/u_threads.h
@@ -0,0 +1,24 @@
+#include "tx_api.h"
+#include <stdint.h>
+#include <stdio.h>
+
+/* Initializes all threads. Called from app_threadx.c */
+uint8_t threads_init(TX_BYTE_POOL *byte_pool);
+
+typedef struct {
+    /* PUBLIC: Thread Configuration Settings */
+    /* Set these when defining an instance of this struct. */
+    const CHAR      *name;                /* Name of Thread */
+    const ULONG     thread_input;         /* Thread Input. You can put whatever you want in here. Defaults to zero. */
+    const ULONG     size;                 /* Stack Size (in bytes) */
+    const UINT      priority;             /* Priority */
+    const UINT      threshold;            /* Preemption Threshold */
+    const ULONG     time_slice;           /* Time Slice */
+    const UINT      auto_start;           /* Auto Start */
+    const UINT      sleep;                /* Sleep (in ticks) */
+    VOID            (*function)(ULONG);   /* Thread Function */
+
+    /* PRIVATE: Internal implementation - DO NOT ACCESS DIRECTLY */
+    /* (should only be accessed by functions in u_threads.c) */
+    TX_THREAD _TX_THREAD;
+} thread_t;
