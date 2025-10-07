32388f114e6c0540fcd63c250c25583d9c631d21
bjackson312006 <blakejackson312006@gmail.com>
2025-10-07 19:10:10 -0400

    no more clang format

--- NetX/inc/u_nx_ethernet.h
+++ NetX/inc/u_nx_ethernet.h
@@ -1,6 +1,8 @@
 #ifndef _U_NX_ETHERNET_H
 #define _U_NX_ETHERNET_H
 
+// clang-format off
+
 /*
 *   NOTE: This file can only be used in projects that include NetXDuo.
 */
@@ -9,21 +11,20 @@
 #include "nx_api.h"
 
 /* CONFIG */
-#define ETH_UDP_PORT	 2006 /* UDP port for communication */
-#define ETH_MESSAGE_SIZE 8 /* Maximum ethernet message size in bytes. */
-#define ETH_MAX_PACKETS \
-	10 /* Maximum number of packets we wanna handle simultaneously */
-#define ETH_NUMBER_OF_NODES 8 /* Number of nodes in the network. */
+#define ETH_UDP_PORT	    2006 /* UDP port for communication */
+#define ETH_MESSAGE_SIZE    8    /* Maximum ethernet message size in bytes. */
+#define ETH_MAX_PACKETS     10   /* Maximum number of packets we wanna handle simultaneously */
+#define ETH_NUMBER_OF_NODES 8    /* Number of nodes in the network. */
 
 typedef enum {
-	VCU = (1 << 0), // 0b00000001
+	VCU = (1 << 0),     // 0b00000001
 	COMPUTE = (1 << 1), // 0b00000010
-	TPU = (1 << 2), // 0b00000100
-	MSB1 = (1 << 3), // 0b00001000
-	MSB2 = (1 << 4), // 0b00010000
-	MSB3 = (1 << 5), // 0b00100000
-	MSB4 = (1 << 6), // 0b01000000
-	NODE8 = (1 << 7), // 0b10000000
+	TPU = (1 << 2),     // 0b00000100
+	MSB1 = (1 << 3),    // 0b00001000
+	MSB2 = (1 << 4),    // 0b00010000
+	MSB3 = (1 << 5),    // 0b00100000
+	MSB4 = (1 << 6),    // 0b01000000
+	NODE8 = (1 << 7),   // 0b10000000
 } ethernet_node_t;
 #define ETH_IP(node) IP_ADDRESS(239, 0, 0, node)
 /* END CONFIG */
@@ -37,12 +38,8 @@ typedef struct {
 } ethernet_message_t;
 
 /* Function Pointers (for initialization). */
-typedef void (*DriverFunction)(
-	NX_IP_DRIVER
-		*); /* User-supplied network driver used to send and receive IP packets. */
-typedef void (*OnRecieve)(
-	ethernet_message_t
-		message); /* User-supplied function that will be called whenever an ethernet message is recieved. */
+typedef void (*DriverFunction)(NX_IP_DRIVER *); /* User-supplied network driver used to send and receive IP packets. */
+typedef void (*OnRecieve)(ethernet_message_t message); /* User-supplied function that will be called whenever an ethernet message is recieved. */
 
 /**
  * @brief Initializes the NetX ethernet system in a repo.
@@ -51,8 +48,7 @@ typedef void (*OnRecieve)(
  * @param on_recieve User-supplied function to be called whenever an ethernet message is recieved. The function's only parameter is an ethernet_message_t instance containing the recieved message.
  * @return Status.
  */
-uint8_t ethernet_init(ethernet_node_t node_id, DriverFunction driver,
-		      OnRecieve on_recieve);
+uint8_t ethernet_init(ethernet_node_t node_id, DriverFunction driver, OnRecieve on_recieve);
 
 /**
  * @brief Creates an ethernet message. Can be send with ethernet_send_message(), or added to a queue.
@@ -62,9 +58,7 @@ uint8_t ethernet_init(ethernet_node_t node_id, DriverFunction driver,
  * @param data_length Length of the data in bytes.
  * @return The created ethernet message.
  */
-ethernet_message_t ethernet_create_message(uint8_t message_id,
-					   ethernet_node_t recipient_id,
-					   uint8_t *data, uint8_t data_length);
+ethernet_message_t ethernet_create_message(uint8_t message_id, ethernet_node_t recipient_id, uint8_t *data, uint8_t data_length);
 
 /**
  * @brief Sends an ethernet message.
@@ -73,4 +67,5 @@ ethernet_message_t ethernet_create_message(uint8_t message_id,
  */
 uint8_t ethernet_send_message(ethernet_message_t *message);
 
+// clang-format on
 #endif /* u_nx_ethernet.h */
