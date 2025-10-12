01374147699dd8d40b1f5b173a0f83df6e7a5ebf
Jack Rubacha <rubacha.jack03@gmail.com>
2025-10-12 16:12:37 -0400

    removal (#327)

--- cangen/archive/Messages.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from .CANMsg import CANMsg
-
-@dataclass
-class Messages:
-      '''
-      Represents a list of CAN messages, used to create a container for each YAML
-      '''
-      msgs: list[CANMsg]
--- cangen/archive/__init__.py
+++
@@ -1,7 +0,0 @@
-from cangen.RustSynthFromJSON import RustSynthFromJSON
-from cangen.CANMsg import CANMsg
-from cangen.CANMsg import EncodableCANMsg
-from cangen.CANField import NetField
-from cangen.Format import Format
-from cangen.Messages import Messages
-from cangen.Result import Result
