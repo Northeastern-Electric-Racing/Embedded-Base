2babe038f47b70fb2aa2ed2d3e0ed182ed4d27a7
Tavi <tavishi_bhatia@outlook.com>
2025-10-30 21:29:28 -0400

    formatted the driver files

--- general/include/honeywellSSC.h
+++ general/include/honeywellSSC.h
@@ -31,29 +31,33 @@
 #define SSC_I2C_ADDR 0x28 // Output type 2
 
 // STATUS BIT VALUES (bits S1 S0 from first byte)
-#define SSC_STATUS_NORMAL       0x00 // normal operation, valid data 
-#define SSC_STATUS_COMMAND_MODE 0x01 // used for programming, should not be seen in normal operation
-#define SSC_STATUS_STALE_DATA   0x02 // data is old
-#define SSC_STATUS_DIAGNOSTIC   0x03 // hardware error
+#define SSC_STATUS_NORMAL 0x00 // normal operation, valid data
+#define SSC_STATUS_COMMAND_MODE \
+	0x01 // used for programming, should not be seen in normal operation
+#define SSC_STATUS_STALE_DATA 0x02 // data is old
+#define SSC_STATUS_DIAGNOSTIC 0x03 // hardware error
 
 // BIT MASKS
-#define SSC_STATUS_MASK    0xC000 // Bits 14-15 for 2-bit status in first 2 bytes
-#define SSC_PRESSURE_MASK  0x3FFF // Bits 0-13 for 14-bit pressure data in first 2 bytes
-#define SSC_TEMP_MASK      0xFFE0 // Bits 6-16 for 11-bit temperature data in last 2 bytes
+#define SSC_STATUS_MASK 0xC000 // Bits 14-15 for 2-bit status in first 2 bytes
+#define SSC_PRESSURE_MASK \
+	0x3FFF // Bits 0-13 for 14-bit pressure data in first 2 bytes
+#define SSC_TEMP_MASK \
+	0xFFE0 // Bits 6-16 for 11-bit temperature data in last 2 bytes
 
-#define SSC_DATA_LENGTH 4 // first 2 bytes for pressure (and status), 2 bytes for temperature (and junk)
+#define SSC_DATA_LENGTH \
+	4 // first 2 bytes for pressure (and status), 2 bytes for temperature (and junk)
 
 /*
 Pressure is calculated from the reading as: 
 Pressure = ((output - output_min) * (pressure_max - pressure_min)) / (output_max - output_min) + pressure_min
 The following are the constants for that equation
 */
-// TRANSFER FUNCTION VALUES (2^14 counts) 
-#define SSC_OUTPUT_MIN 0  // 0% of 2^14
+// TRANSFER FUNCTION VALUES (2^14 counts)
+#define SSC_OUTPUT_MIN 0 // 0% of 2^14
 #define SSC_OUTPUT_MAX 16384 // 100% of 2^14
 
 // PRESSURE RANGE VALUES (0 to 60 psi)
-#define SSC_PRESSURE_MIN 0.0f  // 0 psi
+#define SSC_PRESSURE_MIN 0.0f // 0 psi
 #define SSC_PRESSURE_MAX 60.0f // 60 psi
 
 /*
@@ -63,27 +67,31 @@ The following are constants for that equation
 */
 // TEMPERATURE CONVERSION CONSTANTS
 #define SSC_TEMP_COUNTS_MAX 2047.0f // 11-bit max value
-#define SSC_TEMP_OFFSET -50.0f      // Offset in equation
-#define SSC_TEMP_SCALE 200.0f       // Scale factor in equation
+#define SSC_TEMP_OFFSET	    -50.0f // Offset in equation
+#define SSC_TEMP_SCALE	    200.0f // Scale factor in equation
 
 // Function pointer
-typedef int (*ReadPtr)(uint16_t dev_addr, uint8_t *data, uint16_t data_size); //  if data_size == 2: just reads pressure data, if data_size == 4 reads temp data too
+typedef int (*ReadPtr)(
+	uint16_t dev_addr, uint8_t *data,
+	uint16_t data_size); //  if data_size == 2: just reads pressure data, if data_size == 4 reads temp data too
 // I think the sensor is read-only so no WritePtr needed
 
 // struct that records the current information
 typedef struct {
-    uint16_t dev_addr;
-    ReadPtr read;
-    uint8_t min_pressure;
-    uint8_t max_pressure;
+	uint16_t dev_addr;
+	ReadPtr read;
+	uint8_t min_pressure;
+	uint8_t max_pressure;
 } honeywellSSC_t;
 
 // sets stuff up
-void honeywellSSC_init(honeywellSSC_t *ssc, ReadPtr read, uint16_t dev_addr, uint8_t min_pressure, uint8_t max_pressure);
+void honeywellSSC_init(honeywellSSC_t *ssc, ReadPtr read, uint16_t dev_addr,
+		       uint8_t min_pressure, uint8_t max_pressure);
 
 // Reads the data into the provided data pointer
 // expects honeywellSSC_t and a pointer to a uint8_t array of length 2 or 4
-int honeywellSSC_read_data(honeywellSSC_t *ssc, uint8_t *data, uint8_t data_size);
+int honeywellSSC_read_data(honeywellSSC_t *ssc, uint8_t *data,
+			   uint8_t data_size);
 
 // Reads the status message
 void honeywellSSC_read_status(uint16_t data, uint8_t *result);
--- general/src/honeywellSSC.c
+++ general/src/honeywellSSC.c
@@ -7,102 +7,127 @@
 #include "honeywellSSC.h"
 
 // sets stuff up
-void honeywellSSC_init(honeywellSSC_t *ssc, ReadPtr read, uint16_t dev_addr, uint8_t min_pressure, uint8_t max_pressure) {
-    ssc->read = read;
-    ssc->dev_addr = dev_addr;
-    ssc->min_pressure = min_pressure;
-    ssc->max_pressure = max_pressure;
+void honeywellSSC_init(honeywellSSC_t *ssc, ReadPtr read, uint16_t dev_addr,
+		       uint8_t min_pressure, uint8_t max_pressure)
+{
+	ssc->read = read;
+	ssc->dev_addr = dev_addr;
+	ssc->min_pressure = min_pressure;
+	ssc->max_pressure = max_pressure;
 }
 
 // Reads the data into the provided data pointer
 // expects honeywellSSC_t and a pointer to a uint8_t array of length 2 or 4
-int honeywellSSC_read_data(honeywellSSC_t *ssc, uint8_t *data, uint8_t data_size) {
-    if (data_size != 2 && data_size != 4) {
-        return -1;
-    }
-    return ssc->read(ssc->dev_addr, data, data_size);
+int honeywellSSC_read_data(honeywellSSC_t *ssc, uint8_t *data,
+			   uint8_t data_size)
+{
+	if (data_size != 2 && data_size != 4) {
+		return -1;
+	}
+	return ssc->read(ssc->dev_addr, data, data_size);
 }
 
 // Reads the status message
-void honeywellSSC_read_status(uint16_t data, uint8_t *result) {
-    uint16_t status = (data & SSC_STATUS_MASK) >> 14;
-    *result = (uint8_t)status;
+void honeywellSSC_read_status(uint16_t data, uint8_t *result)
+{
+	uint16_t status = (data & SSC_STATUS_MASK) >> 14;
+	*result = (uint8_t)status;
 }
 
 // Reads the pressure in psi
-int honeywellSSC_read_pressure(honeywellSSC_t *ssc, float *result) {
-    uint8_t data_array[2];
+int honeywellSSC_read_pressure(honeywellSSC_t *ssc, float *result)
+{
+	uint8_t data_array[2];
 
-    int success_msg = honeywellSSC_read_data(ssc, data_array, 2);
-    if (success_msg != 0) {
-        return success_msg;
-    }
+	int success_msg = honeywellSSC_read_data(ssc, data_array, 2);
+	if (success_msg != 0) {
+		return success_msg;
+	}
 
-    uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) | data_array[1];
+	uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) |
+					    data_array[1];
 
-    uint8_t status;
-    honeywellSSC_read_status(status_and_pressure_data, &status);
-    if (status != SSC_STATUS_NORMAL) {
-        return -1;
-    }
+	uint8_t status;
+	honeywellSSC_read_status(status_and_pressure_data, &status);
+	if (status != SSC_STATUS_NORMAL) {
+		return -1;
+	}
 
-    uint16_t pressure_data = status_and_pressure_data & SSC_PRESSURE_MASK;
-    // Pressure = ((output - output_min) * (pressure_max - pressure_min)) / (output_max - output_min) + pressure_min
-    float pressure_reading = (((pressure_data - ssc->min_pressure) * (ssc->max_pressure - ssc->min_pressure)) / (SSC_OUTPUT_MAX - SSC_OUTPUT_MIN) + ssc->min_pressure);
+	uint16_t pressure_data = status_and_pressure_data & SSC_PRESSURE_MASK;
+	// Pressure = ((output - output_min) * (pressure_max - pressure_min)) / (output_max - output_min) + pressure_min
+	float pressure_reading = (((pressure_data - ssc->min_pressure) *
+				   (ssc->max_pressure - ssc->min_pressure)) /
+					  (SSC_OUTPUT_MAX - SSC_OUTPUT_MIN) +
+				  ssc->min_pressure);
 
-    *result = pressure_reading;
-    return 0;
+	*result = pressure_reading;
+	return 0;
 }
 
 // Reads the temperature in celcius
-int honeywellSSC_read_temp(honeywellSSC_t *ssc, float *result) {
-    uint8_t data_array[4];
+int honeywellSSC_read_temp(honeywellSSC_t *ssc, float *result)
+{
+	uint8_t data_array[4];
 
-    int success_msg = honeywellSSC_read_data(ssc, data_array, 4);
-    if (success_msg != 0) {
-        return success_msg;
-    }
+	int success_msg = honeywellSSC_read_data(ssc, data_array, 4);
+	if (success_msg != 0) {
+		return success_msg;
+	}
 
-    uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) | data_array[1];
-    uint16_t temp_raw = ((uint16_t)data_array[2] << 8) | data_array[3];
-    uint16_t temp_data = (temp_raw & SSC_TEMP_MASK) >> 5;
+	uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) |
+					    data_array[1];
+	uint16_t temp_raw = ((uint16_t)data_array[2] << 8) | data_array[3];
+	uint16_t temp_data = (temp_raw & SSC_TEMP_MASK) >> 5;
 
-    uint8_t status;
-    honeywellSSC_read_status(status_and_pressure_data, &status);
-    if (status != SSC_STATUS_NORMAL) {
-        return -1;
-    }
+	uint8_t status;
+	honeywellSSC_read_status(status_and_pressure_data, &status);
+	if (status != SSC_STATUS_NORMAL) {
+		return -1;
+	}
 
-    // Temperature (celcius) = (Output(dec)/2047 * 200) - 50
-    float temperature_reading = ((float)temp_data / SSC_TEMP_COUNTS_MAX) * SSC_TEMP_SCALE + SSC_TEMP_OFFSET;
+	// Temperature (celcius) = (Output(dec)/2047 * 200) - 50
+	float temperature_reading =
+		((float)temp_data / SSC_TEMP_COUNTS_MAX) * SSC_TEMP_SCALE +
+		SSC_TEMP_OFFSET;
 
-    *result = temperature_reading;
-    return 0;
+	*result = temperature_reading;
+	return 0;
 }
 
 // Reads the pressure and temperature in psi and Celcius respectivly
-int honeywellSSC_read_pressure_and_temp(honeywellSSC_t *ssc, float *pressure_result, float *temp_result) {
-    uint8_t data_array[4];
+int honeywellSSC_read_pressure_and_temp(honeywellSSC_t *ssc,
+					float *pressure_result,
+					float *temp_result)
+{
+	uint8_t data_array[4];
 
-    int success_msg = honeywellSSC_read_data(ssc, data_array, 4);
-    if (success_msg != 0) {
-        return success_msg;
-    }
+	int success_msg = honeywellSSC_read_data(ssc, data_array, 4);
+	if (success_msg != 0) {
+		return success_msg;
+	}
 
-    uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) | data_array[1];
-    uint16_t pressure_data = status_and_pressure_data & SSC_PRESSURE_MASK;
-    uint16_t temp_data = ((((uint16_t)data_array[2] << 8) | data_array[3]) & SSC_TEMP_MASK) >> 5;
+	uint16_t status_and_pressure_data = ((uint16_t)data_array[0] << 8) |
+					    data_array[1];
+	uint16_t pressure_data = status_and_pressure_data & SSC_PRESSURE_MASK;
+	uint16_t temp_data = ((((uint16_t)data_array[2] << 8) | data_array[3]) &
+			      SSC_TEMP_MASK) >>
+			     5;
 
-    uint8_t status;
-    honeywellSSC_read_status(status_and_pressure_data, &status);
-    if (status != SSC_STATUS_NORMAL) {
-        return -1;
-    }
+	uint8_t status;
+	honeywellSSC_read_status(status_and_pressure_data, &status);
+	if (status != SSC_STATUS_NORMAL) {
+		return -1;
+	}
 
-    float pressure_reading = (((pressure_data - ssc->min_pressure) * (ssc->max_pressure - ssc->min_pressure)) / (SSC_OUTPUT_MAX - SSC_OUTPUT_MIN) + ssc->min_pressure);
-    float temperature_reading = ((float)temp_data / SSC_TEMP_COUNTS_MAX) * SSC_TEMP_SCALE + SSC_TEMP_OFFSET;
+	float pressure_reading = (((pressure_data - ssc->min_pressure) *
+				   (ssc->max_pressure - ssc->min_pressure)) /
+					  (SSC_OUTPUT_MAX - SSC_OUTPUT_MIN) +
+				  ssc->min_pressure);
+	float temperature_reading =
+		((float)temp_data / SSC_TEMP_COUNTS_MAX) * SSC_TEMP_SCALE +
+		SSC_TEMP_OFFSET;
 
-    *pressure_result = pressure_reading;
-    *temp_result = temperature_reading;
-    return 0;
+	*pressure_result = pressure_reading;
+	*temp_result = temperature_reading;
+	return 0;
 }
