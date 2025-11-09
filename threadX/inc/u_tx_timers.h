b6645f877e13e967ac6d7f56ec4e3cd5d0e9e2f0
bjackson312006 <blakejackson312006@gmail.com>
2025-11-09 17:02:32 -0500

    Added API for ThreadX timers

---
+++ threadX/inc/u_tx_timers.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// clang-format off
+
+#include "tx_api.h"
+#include <stdint.h>
+
+typedef enum {
+    ONESHOT,    /* Timer runs once. */
+    PERIODIC    /* Timer runs periodically based on the 'duration' setting. */
+} timer_type_t;
+
+typedef struct {
+    /* PUBLIC: Thread Configuration Settings */
+	/* Set these when defining an instance of this struct. */
+    const CHAR *name;        /* Name of the timer. */ 
+    VOID (*callback)(ULONG); /* Callback to be ran when the timer expires. */
+    ULONG duration;          /* The time until the timer expires (in ticks). */
+    ULONG callback_input;    /* Optional setting. Input for the callback function. Just set to '0' if you don't need to use. */
+    timer_type_t type;       /* Type of timer. See 'timer_type_t'. */
+    bool auto_activate;      /* Whether or not the timer should auto-activate. */
+
+    /* PRIVATE: Internal implementation - DO NOT ACCESS DIRECTLY */
+	/* (should only be accessed by functions in u_tx_timers.c) */
+    TX_TIMER TX_TIMER_;      /* The actual timer instance. */
+} timer_t;
+
+/* API */
+int timer_init(timer_t* timer);
+int timer_start(timer_t* timer);
+int timer_stop(timer_t* timer);
+int timer_reset(timer_t* timer);
+int timer_restart(timer_t* timer);
+int timer_getRemainingTicks(uint32_t *remaining);
+
+// clang-format on
---
+++ threadX/src/u_tx_timers.c
@@ -0,0 +1,156 @@
+#include "u_tx_timers.h"
+#include "u_tx_debug.h"
+
+// clang-format off
+
+/* Check if a timer is active. */
+int _is_timer_active(timer_t* timer, bool* active) {
+    /* Get the active indication. */
+    UINT is_active;
+    int status = tx_timer_info_get(&timer->TX_TIMER_, (CHAR**)TX_NULL, &is_active, (ULONG*)TX_NULL, (ULONG*)TX_NULL, (TX_TIMER**)TX_NULL);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_info_get (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    /* Check the indication. */
+    if(is_active == TX_TRUE) {
+        *active = true;
+    } else {
+        *active = false;
+    }
+
+    return U_SUCCESS;
+}
+
+/* Initializes a timer. */
+int timer_init(timer_t* timer) {
+    /* Set reschedule ticks setting. */
+    ULONG reschedule_ticks;
+    if(timer->type == ONESHOT) {
+        reschedule_ticks = 0; // If it's a one-shot timer, don't reschedule the timer.
+    } else {
+        reschedule_ticks = timer->duration; // If it's a periodic timer, set the timer to go off every 'duration' ticks.
+    }
+
+    /* Set auto-activate setting. */
+    UINT auto_activate;
+    if(timer->auto_activate) {
+        auto_activate = TX_AUTO_ACTIVATE; // The timer will start automatically at initialization.
+    } else {
+        auto_activate = TX_NO_ACTIVATE; // The timer must be manually started after initialization.
+    }
+
+    /* Create the timer. */
+    int status = tx_timer_create(&timer->TX_TIMER_, (CHAR*)timer->name, timer->callback, timer->callback_input, timer->duration, reschedule_ticks, auto_activate);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to create timer (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    /* Return successful. */
+    PRINTLN_INFO("Ran Timer::init() (Timer: %s).", timer->name);
+    return U_SUCCESS;
+}
+
+/* Starts a timer. */
+int timer_start(timer_t* timer) {
+    /* Get active status. */
+    bool is_active = false;
+    int status = _is_timer_active(timer, &is_active);
+    if(status != U_SUCCESS) {
+        PRINTLN_ERROR("Failed to get the activation status of a timer (Timer: %s).", timer->name);
+        return U_ERROR;
+    }
+
+    /* Check active status. */
+    if(is_active) {
+        PRINTLN_ERROR("Tried to start a timer that is already active (Timer: %s).", timer->name);
+        return U_ERROR;
+    }
+
+    /* Activate the timer. */
+    status = tx_timer_activate(&timer->TX_TIMER_);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_activate() (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    return U_SUCCESS;
+}
+
+/* Stops a timer. If the timer is already deactivated, this function will have no effect. */
+int timer_stop(timer_t* timer) {
+    /* Deactivate the timer. */
+    int status = tx_timer_deactivate(&timer->TX_TIMER_);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_deactivate() (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+    return U_SUCCESS;
+}
+
+/* Resets a timer back to its starting position. Does not automatically start the timer after resetting (use Timer::restart() for that.) */
+int timer_reset(timer_t* timer) {
+    /* Deactivate the timer. */
+    int status = tx_timer_deactivate(&timer->TX_TIMER_);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_deactivate() (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    /* Set reschedule ticks setting. */
+    ULONG reschedule_ticks;
+    if(timer->one_shot) {
+        reschedule_ticks = 0; // If it's a one-shot timer, don't reschedule the timer.
+    }
+    else {
+        reschedule_ticks = timer->duration; // If it's a periodic timer, set the timer to go off every 'duration' ticks.
+    }
+
+    /* Change the timer. */
+    status = tx_timer_change(&timer->TX_TIMER_, timer->duration, reschedule_ticks);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_change() (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    return U_SUCCESS;
+}
+
+/* Restarts a timer. */
+int timer_restart(timer_t* timer) {
+    /* Reset the timer. */
+    int status = timer_reset(timer);
+    if(status != U_SUCCESS) {
+        PRINTLN_ERROR("Failed to call timer_reset() when restarting timer (Timer: %s).", timer->name);
+        return U_ERROR;
+    }
+
+    /* Start the timer. */
+    status = timer_start(timer);
+    if(status != U_SUCCESS) {
+        PRINTLN_ERROR("Failed to call timer_start() when restarting timer (Timer: %s).", timer->name);
+        return U_ERROR;
+    }
+
+    return U_SUCCESS;
+}
+
+/* Gets the remaining number of ticks until the timer's expiration. */
+int timer_getRemainingTicks(timer_t* timer, uint32_t* remaining) {
+    /* Get the remaining ticks status. */
+    ULONG remaining_ticks;
+    int status = tx_timer_info_get(&timer->TX_TIMER_, (CHAR**)TX_NULL, (UINT*)TX_NULL, &remaining_ticks, (ULONG*)TX_NULL, (TX_TIMER**)TX_NULL);
+    if(status != TX_SUCCESS) {
+        PRINTLN_ERROR("Failed to call tx_timer_info_get (Status: %d/%s, Timer: %s).", status, tx_status_toString(status), timer->name);
+        return U_ERROR;
+    }
+
+    /* Set the remaining ticks. */
+    *remaining = (uint32_t)(remaining_ticks);
+
+    return U_SUCCESS;
+}
+
+// clang-format on
