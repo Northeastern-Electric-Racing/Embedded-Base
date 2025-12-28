40e151d3a85f7e35facb69c4a79a7c799bc103aa
bjackson312006 <blakejackson312006@gmail.com>
2025-12-28 13:59:37 -0500

    Added mutex-isOwned function

--- threadX/src/u_tx_mutex.c
+++ threadX/src/u_tx_mutex.c
@@ -39,4 +39,17 @@ uint8_t mutex_put(mutex_t *mutex)
 	return U_SUCCESS;
 }
 
+/* Checks if a mutex is owned by the current thread. */
+bool mutex_isOwned(mutex_t *mutex) {
+	TX_THREAD* mutex_thread; // Thread that currently owns the mutex.
+	uint8_t status = tx_mutex_info_get(&mutex->_TX_MUTEX, TX_NULL, TX_NULL, &mutex_thread, TX_NULL, TX_NULL, TX_NULL);
+	if(status != TX_SUCCESS) {
+		PRINTLN_ERROR("Failed to call tx_mutex_info_get() (Status: %d/%s, Mutex: %s).", status, tx_status_toString(status), mutex->name);
+		return false;
+	}
+
+	TX_THREAD* current_thread = tx_thread_identify(); // The current active thread (i.e., the thread that called mutex_isOwned()).
+	return (mutex_thread == current_thread); // If mutex_thread is the same as current_thread, return true. If not, return false.
+}
+
 // clang-format on
