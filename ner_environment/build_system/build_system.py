ccdc108f292682600512d9c0932d0dc89194550e
bjackson312006 <blakejackson312006@gmail.com>
2026-01-06 14:23:37 -0500

    disable that temporarily

--- ner_environment/build_system/build_system.py
+++ ner_environment/build_system/build_system.py
@@ -74,7 +74,7 @@ def build(profile: str = typer.Option(None, "--profile", "-p", callback=unsuppor
             run_command_docker(f"make -j{os.cpu_count()}", stream_output=True)
 
     if not clean:
-        fix_compile_commands()
+        #fix_compile_commands()
 
 # ==============================================================================
 # Clang command
