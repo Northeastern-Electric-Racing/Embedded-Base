e004cea9bae46ea06f998361da484e84d58cf76c
KSMehta11 <kaushikmehta119@gmail.com>
2026-01-26 19:18:27 -0800

    Add required macro

--- middleware/src/c_utils.c
+++ middleware/src/c_utils.c
@@ -2,6 +2,9 @@
 #include <assert.h>
 #include <math.h>
 
+/* Epsilon for float comparisons */
+#define FLOAT_EPSILON (0.0001f)
+
 void endian_swap(void *ptr, int size)
 {
 	char *p = (char *)ptr;
@@ -23,7 +26,7 @@ unsigned char reverse_bits(unsigned char b)
 
 float linear_interpolate(float x, float x1, float x2, float y1, float y2)
 {
-	assert(fabs(x2 - x1) > 0.0001f);
+	assert(fabs(x2 - x1) > FLOAT_EPSILON);
 
 	return y1 + ((x - x1) * (y2 - y1) / (x2 - x1));
 }
