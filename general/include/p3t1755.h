f6b963142cb25da6978239e0a5b8d24ecef9bd25
Caio DaSilva <dasilva.cai@northeastern.edu>
2026-03-20 15:23:13 -0400

    fix header

--- general/include/p3t1755.h
+++ general/include/p3t1755.h
@@ -65,7 +65,7 @@ typedef struct {
   ReadPtr read;
 } p3t1755_t;
 
-static inline float p3t1755_raw_to_celsius(uint16_t raw);
+float p3t1755_raw_to_celsius(uint16_t raw);
 
 void p3t1755_init(p3t1755_t *p3t, WritePtr write, ReadPtr read,
                   uint16_t dev_addr);
