c03ca7b81fb9d6025dab8676955e4cd360c1b369
Jack Rubacha <rubacha.jack03@gmail.com>
2026-06-27 20:04:20 -0400

    alt rtc

--- NetX/src/u_rtc.c
+++ NetX/src/u_rtc.c
@@ -4,63 +4,24 @@
 #include "u_tx_debug.h"
 #include <time.h>
 
+/// This file implements the PTP client callback to manage time.
+/// This is where the interface between the PTP client and the timebase is.
+/// This was written using RM0481 and UM3131.
+///
+/// The timebase for the clock used is listed as clk_ptp_ref_i.
+/// The PTP Offload feature is NOT utilized because it is very half-baked.
+/// Instead we simply use the internal clock reference and a userspace UDP impl.
+///
+/// TUNING: The status of this operation can be found by using eth_ptp_pps_out.
+///
+/// Docs: This mostly follows the procedure in page RM0481 2792 57.9.9
+/// This uses coarse timestamping binary rollover mode
+///
+
+extern ETH_HandleTypeDef heth;
+
 #define PTP_UTC_OFFSET 0 // UTC 0
-extern RTC_HandleTypeDef hrtc;
 
-UINT interrupt_save; // do not remove
-
-static UINT us_to_second_ticks(ULONG us, UINT second_fractions)
-{
-	// Second fraction = SS / (PREDIV_S + 1)
-	return (uint64_t)us * (second_fractions + 1) / 1000000L;
-}
-
-static ULONG second_ticks_to_us(UINT second_ticks, UINT second_fractions)
-{
-	return (uint64_t)1000000L * second_ticks / (second_fractions + 1);
-}
-
-static void set_subsecond(UINT rtc_sub_second_tick, UINT second_fractions, ULONG nanoseconds)
-{
-	if (rtc_sub_second_tick > second_fractions) {
-		PRINTLN_ERROR("rtc SS overflow");
-	}
-
-	UINT rtp_sub_second_tick = us_to_second_ticks(
-		nanoseconds / 1000, second_fractions);
-
-	UINT offset_tick = 0; // ticks to go backwards
-	UINT offset_ahead_1s = RTC_SHIFTADD1S_RESET;
-	if (rtc_sub_second_tick >= rtp_sub_second_tick) { // local ahead
-		offset_tick = rtc_sub_second_tick - rtp_sub_second_tick;
-	} else { // local behind
-		offset_ahead_1s = RTC_SHIFTADD1S_SET;
-		UINT delta = rtp_sub_second_tick - rtc_sub_second_tick;
-		offset_tick = (second_fractions + 1) - delta;
-	}
-	HAL_RTCEx_SetSynchroShift(&hrtc, offset_ahead_1s, offset_tick);
-}
-
-static UINT rtc_to_nx_time(RTC_TimeTypeDef *rtc_time, RTC_DateTypeDef *rtc_date, NX_PTP_TIME *time_ptr) {
-    // helpful way to get UNIX time from RTC so we can give it to NetX layer
-	time_t current_time_unix = { 0 };
-	struct tm tim = {0};
-	tim.tm_year = rtc_date->Year + 100;
-	tim.tm_mon = rtc_date->Month - 1;
-	tim.tm_mday = rtc_date->Date;
-	tim.tm_hour = rtc_time->Hours;
-	tim.tm_min = rtc_time->Minutes;
-	tim.tm_sec = rtc_time->Seconds;
-	current_time_unix = mktime(&tim);
-
-	time_ptr->second_high = 0; // todo fix by 2038
-	time_ptr->second_low = (ULONG) current_time_unix;
-	time_ptr->nanosecond = 1000 * second_ticks_to_us(
-		rtc_time->SecondFraction - rtc_time->SubSeconds,
-		rtc_time->SecondFraction); // pull directly from rtc
-
-	return 0;
-}
 
 UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 				       UINT operation, NX_PTP_TIME *time_ptr,
@@ -68,97 +29,89 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 				       VOID *callback_data)
 {
 	NX_PTP_DATE_TIME current_date_time = { 0 };
-	RTC_TimeTypeDef rtc_time = {0};
-	RTC_DateTypeDef rtc_date = {0};
+	HAL_StatusTypeDef status;
 
 	switch (operation) {
 		case NX_PTP_CLIENT_CLOCK_INIT:
+			TX_DISABLE
+
+			// see 57.9.9
+
+			// Mask the Timestamp Trigger interrupt
+			CLEAR_BIT(heth->Instance->MACIER, ETH_MACIER_TSIE);
+			SET_BIT(heth->Instance->MACTSCR, ETH_MACTSCR_TSENA);
+
+			SET_BIT(heth->Instance->MACTSCR, ETH_MACTSCR_TSCTRLSSR); // 1 ns
+
+			// each step is 1ns, with a clock of clk_ptp_ref_i
+			// clk_ptp_ref_i comes from pll1_q_clk (eth clock) (pg 456)
+			// this cool API gives us the frequency (in hz)
+			uint32_t clk_ptp_ref_i = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_ETH);
+			uint8_t period_ns = 1000000000 / clk_ptp_ref_i;
+			MODIFY_REG(heth->Instance->MACSSIR, ETH_MACMACSSIR_SNSINC, period_ns << ETH_MACMACSSIR_SNSINC_Pos);
+
+			TX_RESTORE
 			break;
 
 		case NX_PTP_CLIENT_CLOCK_SET:
+			/// A large jump in time
 			TX_DISABLE
 
-			UINT status = nx_ptp_client_utility_convert_time_to_date(
-				time_ptr, -PTP_UTC_OFFSET, &current_date_time);
+			// use overwrite, not add
+			WRITE_REG(heth->Instance->MACSTNUR, time_ptr->nanosecond);
 
-			rtc_time = (RTC_TimeTypeDef) {
-				.Hours = current_date_time.hour,
-				.Minutes = current_date_time.minute,
-				.Seconds = current_date_time.second,
-				.TimeFormat = 0,
-			};
+			// WARNING travelers of future: this code suffers from the year 2106 problem
+			// Use seconds_high to account for this, but note the silicon also has this issue
+			WRITE_REG(heth->Instance->MACSTSUR, time_ptr->second_low);
 
-			rtc_date = (RTC_DateTypeDef) {
-				.Year = current_date_time.year % 100,
-				.Month = current_date_time.month,
-				.Date = current_date_time.day,
-				.WeekDay = current_date_time.weekday,
-			};
 
-			// PRINTLN_INFO("GOT TIME SET: %d, sending NX time (%lu, %lu) from year %d, month %d, day %d, hour %d, minute %d, second %d",
-			//         status,
-			//         time_ptr->second_high, time_ptr->second_low,
-			//         current_date_time.year, current_date_time.month, current_date_time.day, current_date_time.hour, current_date_time.minute, current_date_time.second);
-			// PRINTLN_INFO("GOT TIME SET, sending  RTC from year %d, month %d, day %d, hour %d, minute %d, second %d",
-			//         rtc_date.Year, rtc_date.Month, rtc_date.Date, rtc_time.Hours, rtc_time.Minutes, rtc_time.Seconds);
+			SET_BIT(heth->Instance->MACTSCR, ETH_MACTSCR_TSINIT);
 
-			HAL_RTC_SetTime(&hrtc, &rtc_time, RTC_FORMAT_BIN);
-			HAL_RTC_SetDate(&hrtc, &rtc_date, RTC_FORMAT_BIN);
+			while (GET_BIT(heth->Instance->MACTSCR, ETH_MACTSCR_TSINIT)) {}
+			// clear the seconds register now so we dont need to do it for a subsecond update
+			CLEAR_REG(heth->Instance->MACSTUR);
 
-			// After SetTime, SubSeconds is reset to SecondFraction (0 elapsed).
-			// Shift from 0 elapsed ticks to wherever PTP nanosecond says we should be.
-			RTC_TimeTypeDef after_set = { 0 };
-			HAL_RTC_GetTime(&hrtc, &after_set, RTC_FORMAT_BIN); // to get a valid SecondFraction
-			set_subsecond(0, after_set.SecondFraction, time_ptr->nanosecond);
-
-			// dummy get date
-			HAL_RTC_GetDate(&hrtc, &rtc_date, RTC_FORMAT_BIN);
 			TX_RESTORE
 			break;
 		case NX_PTP_CLIENT_CLOCK_PACKET_TS_EXTRACT: // FALL THROUGH
 		case NX_PTP_CLIENT_CLOCK_GET:
+			/// Retrieve time, usually for packet timestamping
 			TX_DISABLE
 
-			HAL_RTC_GetTime(&hrtc, &rtc_time, RTC_FORMAT_BIN);
-			HAL_RTC_GetDate(&hrtc, &rtc_date, RTC_FORMAT_BIN);
-
-			rtc_to_nx_time(&rtc_time, &rtc_date, time_ptr);
-
-			// PRINTLN_INFO("GOT TIME REQ, recv NX from year %d, month %d, day %d, hour %d, minute %d, second %d",
-			//         rtc_date.Year, rtc_date.Month, rtc_date.Date, rtc_time.Hours, rtc_time.Minutes, rtc_time.Seconds);
-
+			time_ptr->nanosecond = READ_REG(heth->Instance->MACSTNR);
+            time_ptr->second_low = READ_REG(heth->Instance->MACSTSR);
 			TX_RESTORE
 			break;
 		case NX_PTP_CLIENT_CLOCK_ADJUST:
+			/// A small adjustment in time (<1 second)
 			TX_DISABLE
+			// use add, not overwrite
+			if (time_ptr->nanosecond >= 0) {
+				// this will automatically clear the ADDSUB so we add
+				WRITE_REG(heth->Instance->MACSTNUR, time_ptr->nanosecond);
+			} else {
+				// take complement starting from one million
+				WRITE_REG(heth->Instance->MACSTSUR, 0x3B9AC9FF - time_ptr->second_low)
+				SET_BIT(heth->Instance->MACSTNUR, ETH_MACSTNUR_ADDSUB);
+			}
 
-			HAL_RTC_GetTime(&hrtc, &rtc_time, RTC_FORMAT_BIN);
 
-			set_subsecond(
-				rtc_time.SecondFraction - rtc_time.SubSeconds,
-				rtc_time.SecondFraction, time_ptr->nanosecond); // (between 0 and PREDIV_S (aka SecondFraction), counting up)
-
-
-			// dummy get date
-			HAL_RTC_GetDate(&hrtc, &rtc_date, RTC_FORMAT_BIN);
+			SET_BIT(heth->Instance->MACTSCR, ETH_MACTSCR_TSUPDT);
 
 			TX_RESTORE
 			break;
 		case NX_PTP_CLIENT_CLOCK_PACKET_TS_PREPARE:
-		    HAL_RTC_GetTime(&hrtc, &rtc_time, RTC_FORMAT_BIN);
-			HAL_RTC_GetDate(&hrtc, &rtc_date, RTC_FORMAT_BIN);
+			/// Push a packet with a timestamp
 
-			rtc_to_nx_time(&rtc_time, &rtc_date, time_ptr);
-
-			// PRINTLN_INFO("GOT TIME REQ, recv NX from year %d, month %d, day %d, hour %d, minute %d, second %d",
-			//         rtc_date.Year, rtc_date.Month, rtc_date.Date, rtc_time.Hours, rtc_time.Minutes, rtc_time.Seconds);
+			time_ptr->nanosecond = READ_REG(heth->Instance->MACSTNR);
+			time_ptr->second_low = READ_REG(heth->Instance->MACSTSR);
 
 			nx_ptp_client_packet_timestamp_notify(client_ptr, packet_ptr, time_ptr);
 			break;
 		case NX_PTP_CLIENT_CLOCK_SOFT_TIMER_UPDATE: // do nothing
 			break;
 		default:
-			PRINTLN_ERROR("How Did We Get Here? (rtc.h)");
+			PRINTLN_ERROR("How Did We Get Here? (rtc)");
 			break;
 	}
 
