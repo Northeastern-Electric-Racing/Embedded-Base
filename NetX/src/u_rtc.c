68195fdfb379421c90a3d1f0021cd230b498877a
Jack Rubacha <rubacha.jack03@gmail.com>
2026-06-28 13:46:22 -0400

    fmt

--- NetX/src/u_rtc.c
+++ NetX/src/u_rtc.c
@@ -31,7 +31,6 @@ extern ETH_HandleTypeDef heth;
 
 #define PTP_UTC_OFFSET 0 // UTC 0
 
-
 UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 				       UINT operation, NX_PTP_TIME *time_ptr,
 				       NX_PACKET *packet_ptr,
@@ -48,7 +47,8 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			CLEAR_BIT(heth.Instance->MACIER, ETH_MACIER_TSIE);
 			SET_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSENA);
 
-			SET_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSCTRLSSR); // 1 ns
+			SET_BIT(heth.Instance->MACTSCR,
+				ETH_MACTSCR_TSCTRLSSR); // 1 ns
 
 			// each step is 1ns, with a clock of clk_ptp_ref_i
 			// clk_ptp_ref_i comes from pll1_q_clk (eth clock) (pg 456)
@@ -61,10 +61,13 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			HAL_RCCEx_GetPLL1ClockFreq(&pll1);
 			if (1000000000 % pll1.PLL1_Q_Frequency != 0) {
 				// Should this be an assertion instead?
-				PRINTLN_ERROR("NOT PERFEECT DIVISOR PTP TIME WILL BE OFF -> CHECK PLL1Q!");
+				PRINTLN_ERROR(
+					"NOT PERFEECT DIVISOR PTP TIME WILL BE OFF -> CHECK PLL1Q!");
 			}
 			uint32_t period_ns = 1000000000 / pll1.PLL1_Q_Frequency;
-			MODIFY_REG(heth.Instance->MACSSIR, ETH_MACMACSSIR_SNSINC, period_ns << ETH_MACMACSSIR_SSINC_Pos);
+			MODIFY_REG(heth.Instance->MACSSIR,
+				   ETH_MACMACSSIR_SNSINC,
+				   period_ns << ETH_MACMACSSIR_SSINC_Pos);
 
 			// placeholder just to get the clock ticking
 			WRITE_REG(heth.Instance->MACSTNUR, 0);
@@ -72,13 +75,13 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			// September 9th, 2001
 			WRITE_REG(heth.Instance->MACSTSUR, 1000000000);
 
-
 			SET_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSINIT);
 
-			while (READ_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSINIT)) {}
+			while (READ_BIT(heth.Instance->MACTSCR,
+					ETH_MACTSCR_TSINIT)) {
+			}
 			// clear the seconds register now so we dont need to do it for a subsecond update
 			CLEAR_REG(heth.Instance->MACSTSUR);
-
 
 			TX_RESTORE
 
@@ -88,16 +91,19 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			TX_DISABLE
 
 			// use overwrite, not add
-			WRITE_REG(heth.Instance->MACSTNUR, time_ptr->nanosecond);
+			WRITE_REG(heth.Instance->MACSTNUR,
+				  time_ptr->nanosecond);
 
 			// WARNING travelers of future: this code suffers from the year 2106 problem
 			// Use seconds_high to account for this, but note the silicon also has this issue
-			WRITE_REG(heth.Instance->MACSTSUR, time_ptr->second_low);
-
+			WRITE_REG(heth.Instance->MACSTSUR,
+				  time_ptr->second_low);
 
 			SET_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSINIT);
 
-			while (READ_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSINIT)) {}
+			while (READ_BIT(heth.Instance->MACTSCR,
+					ETH_MACTSCR_TSINIT)) {
+			}
 			// clear the seconds register now so we dont need to do it for a subsecond update
 			CLEAR_REG(heth.Instance->MACSTSUR);
 
@@ -110,7 +116,7 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			TX_DISABLE
 
 			time_ptr->nanosecond = READ_REG(heth.Instance->MACSTNR);
-            time_ptr->second_low = READ_REG(heth.Instance->MACSTSR);
+			time_ptr->second_low = READ_REG(heth.Instance->MACSTSR);
 			time_ptr->second_high = 0;
 			TX_RESTORE
 			break;
@@ -121,12 +127,15 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			// MACTSUR must be 0 at this point, not wasting CPU time though
 			if (time_ptr->nanosecond >= 0) {
 				// this will automatically clear the ADDSUB so we add
-				WRITE_REG(heth.Instance->MACSTNUR, time_ptr->nanosecond);
+				WRITE_REG(heth.Instance->MACSTNUR,
+					  time_ptr->nanosecond);
 			} else {
 				// take complement starting from one million
-				uint32_t complement = 1000000000 - abs(time_ptr->nanosecond);
+				uint32_t complement =
+					1000000000 - abs(time_ptr->nanosecond);
 				WRITE_REG(heth.Instance->MACSTNUR, complement);
-				SET_BIT(heth.Instance->MACSTNUR, ETH_MACSTNUR_ADDSUB);
+				SET_BIT(heth.Instance->MACSTNUR,
+					ETH_MACSTNUR_ADDSUB);
 			}
 
 			SET_BIT(heth.Instance->MACTSCR, ETH_MACTSCR_TSUPDT);
@@ -140,7 +149,8 @@ UINT nx_ptp_client_hard_clock_callback(NX_PTP_CLIENT *client_ptr,
 			time_ptr->second_low = READ_REG(heth.Instance->MACSTSR);
 			time_ptr->second_high = 0;
 
-			nx_ptp_client_packet_timestamp_notify(client_ptr, packet_ptr, time_ptr);
+			nx_ptp_client_packet_timestamp_notify(
+				client_ptr, packet_ptr, time_ptr);
 			break;
 		case NX_PTP_CLIENT_CLOCK_SOFT_TIMER_UPDATE: // do nothing
 			break;
